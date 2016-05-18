aadedb378a3b13233efaf5414819dd5182b33786
Jeffrey E. Bedard <jefbed@gmail.com>
2016-05-18 03:33:34 -0400

    Added MARK for quick debugging.

--- log.h
+++ log.h
@@ -1,9 +1,12 @@
 #ifndef JBXVT_LOG_H
 #define JBXVT_LOG_H
 
+#include <stdio.h>
+
+#define MARK fprintf(stderr, "MARK: %s:%d\n", __FILE__, __LINE__)
+
 #ifdef DEBUG
 
-#include <stdio.h>
 #define LOG(...) {\
 	fprintf(stderr, "%s:%d> ", __FILE__, __LINE__);\
 	fprintf(stderr, __VA_ARGS__);\
