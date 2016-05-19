e54385e01dee5529abbe35f5037792d942a2743f
Jeffrey E. Bedard <jefbed@gmail.com>
2016-05-19 13:56:49 -0400

    Use XPoint struct.  Reorder declarations per C99 style, latest possible.

--- scr_insert_characters.c
+++ scr_insert_characters.c
@@ -9,39 +9,37 @@
 #include <string.h>
 
 //  Insert count spaces from the current position.
-void scr_insert_characters(int count)
+void scr_insert_characters(int16_t count)
 {
-	int x1, x2, y, width, i;
-
 	if (count > jbxvt.scr.chars.width - jbxvt.scr.current->col)
-		count = jbxvt.scr.chars.width - jbxvt.scr.current->col;
+		  count = jbxvt.scr.chars.width - jbxvt.scr.current->col;
 	if (count <= 0)
-		return;
+		  return;
 
 	home_screen();
 	cursor();
 	check_selection(jbxvt.scr.current->row,jbxvt.scr.current->row);
 	uint8_t * s = jbxvt.scr.current->text[jbxvt.scr.current->row];
 	uint32_t * r = jbxvt.scr.current->rend[jbxvt.scr.current->row];
-	for (i = jbxvt.scr.chars.width - 1;
+	for (int16_t i = jbxvt.scr.chars.width - 1;
 		i >= jbxvt.scr.current->col + count; i--) {
 		s[i] = s[i - count];
 		r[i] = r[i - count];
 	}
 	memset(s + jbxvt.scr.current->col,0,count);
 	memset(r + jbxvt.scr.current->col,0,count);
-	y = MARGIN + jbxvt.scr.current->row * jbxvt.X.font_height;
-	x1 = MARGIN + jbxvt.scr.current->col * jbxvt.X.font_width;
-	x2 = x1 + count * jbxvt.X.font_width;
-	width = (jbxvt.scr.chars.width - count - jbxvt.scr.current->col)
-		* jbxvt.X.font_width;
+	const XPoint p = { .x = MARGIN + jbxvt.scr.current->col
+		* jbxvt.X.font_width, .y = MARGIN + jbxvt.scr.current->row
+			* jbxvt.X.font_height};
+	const uint16_t width = (jbxvt.scr.chars.width - count
+		- jbxvt.scr.current->col) * jbxvt.X.font_width;
 	if (width > 0)
-		XCopyArea(jbxvt.X.dpy,jbxvt.X.win.vt,jbxvt.X.win.vt,
-			jbxvt.X.gc.ne,x1,y,width,jbxvt.X.font_height,x2,y);
-	x1 = MARGIN + jbxvt.scr.current->col * jbxvt.X.font_width;
-	width = count * jbxvt.X.font_width;
-	XClearArea(jbxvt.X.dpy,jbxvt.X.win.vt,x1,y,width,
-		jbxvt.X.font_height,False);
+		  XCopyArea(jbxvt.X.dpy,jbxvt.X.win.vt,
+			  jbxvt.X.win.vt, jbxvt.X.gc.ne,
+			  p.x, p.y, width, jbxvt.X.font_height,
+			  p.x+count*jbxvt.X.font_width, p.y);
+	XClearArea(jbxvt.X.dpy, jbxvt.X.win.vt, p.x, p.y,
+		count * jbxvt.X.font_width, jbxvt.X.font_height, False);
 	jbxvt.scr.current->wrap_next = 0;
 	cursor();
 }
--- scr_insert_characters.h
+++ scr_insert_characters.h
@@ -1,7 +1,9 @@
 #ifndef SCR_INSERT_CHARACTERS_H
 #define SCR_INSERT_CHARACTERS_H
 
+#include <stdint.h>
+
 //  Insert count spaces from the current position.
-void scr_insert_characters(int count);
+void scr_insert_characters(int16_t count);
 
 #endif//!SCR_INSERT_CHARACTERS_H
