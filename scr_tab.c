190c29642d778d59050fab5609acd637c855fb51
Jeffrey E. Bedard <jefbed@gmail.com>
2016-05-19 02:29:36 -0400

    Broke long lines.

--- scr_tab.c
+++ scr_tab.c
@@ -12,11 +12,15 @@ void scr_tab(void)
 	if (jbxvt.scr.current->col == jbxvt.scr.chars.width - 1)
 		return;
 	cursor();
-	check_selection(jbxvt.scr.current->row,jbxvt.scr.current->row);
-	if (jbxvt.scr.current->text[jbxvt.scr.current->row][jbxvt.scr.current->col] == 0)
-		jbxvt.scr.current->text[jbxvt.scr.current->row][jbxvt.scr.current->col] = '\t';
+	check_selection(jbxvt.scr.current->row,
+		jbxvt.scr.current->row);
+	if (jbxvt.scr.current->text[jbxvt.scr.current->row]
+		[jbxvt.scr.current->col] == 0)
+		jbxvt.scr.current->text[jbxvt.scr.current->row]
+				[jbxvt.scr.current->col] = '\t';
 	jbxvt.scr.current->col++;
-	while (jbxvt.scr.current->col % 8 != 0 && jbxvt.scr.current->col
+	while (jbxvt.scr.current->col % 8
+		&& jbxvt.scr.current->col
 		< jbxvt.scr.chars.width - 1)
 		jbxvt.scr.current->col++;
 	cursor();
