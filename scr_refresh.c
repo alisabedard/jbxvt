0fa387f9d590c67bd87702190c758d77c6e716cc
Jeffrey E. Bedard <jefbed@gmail.com>
2016-05-24 04:35:04 -0400

    Remove dead code.

--- scr_refresh.c
+++ scr_refresh.c
@@ -14,18 +14,6 @@
  *  repair after minor exposure events.  */
 void scr_refresh(int x, int y, int width, int height)
 {
-#if 0
-	const uint16_t col1 = constrain((x - MARGIN) / jbxvt.X.font_width,
-		jbxvt.scr.chars.width);
-	const uint16_t col2 = constrain((x + width - MARGIN
-		+ jbxvt.X.font_width - 1) / jbxvt.X.font_width - 1,
-		jbxvt.scr.chars.width);
-	const uint16_t row1 = constrain((y - MARGIN) / jbxvt.X.font_height,
-		jbxvt.scr.chars.height);
-	const uint16_t row2 = constrain((y + height - MARGIN
-		+ jbxvt.X.font_height - 1) / jbxvt.X.font_height - 1,
-		jbxvt.scr.chars.height);
-#endif
 	x -= MARGIN;
 	y -= MARGIN;
 	const uint16_t col1 = constrain(x/jbxvt.X.font_width,
