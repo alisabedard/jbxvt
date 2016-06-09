2b0c4e783f0e90d90ee5db55c5b6ed11be1e11ef
Jeffrey E. Bedard <jefbed@gmail.com>
2016-06-09 00:57:40 -0400

    Define VCOUNT as a block.

--- log.h
+++ log.h
@@ -10,7 +10,9 @@
 #define MARK_I(i) dprintf(STDERR_FILENO, "MARK: " __FILE__ ":%d value: %d\n",\
 	__LINE__, i)
 // Used for simplistic profiling and code path testing:
-#define VCOUNT(v) static uint32_t v; ++v; dprintf(STDERR_FILENO, #v ": %d\n", v);
+#define VCOUNT(v) {\
+	static uint32_t v; ++v; dprintf(STDERR_FILENO, #v ": %d\n", v);\
+}
 
 #ifdef DEBUG
 
