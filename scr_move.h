70bed392aae17aa024d41c4747a52e2eade23bdd
Jeffrey E. Bedard <jefbed@gmail.com>
2016-06-11 03:51:12 -0400

    Use stdint types.

--- scr_move.h
+++ scr_move.h
@@ -1,10 +1,10 @@
 #ifndef SCR_MOVE_H
 #define SCR_MOVE_H
 
-/*  Move the cursor to a new position.  The relative argument is a pair of
- *  flags that specify relative rather than absolute motion.
- */
-void scr_move(int x, int y, int relative);
+#include <stdint.h>
 
+/*  Move the cursor to a new position.  The relative argument is a pair of
+ *  flags that specify relative rather than absolute motion.  */
+void scr_move(const int16_t x, const int16_t y, const uint8_t relative);
 
 #endif//!SCR_MOVE_H
