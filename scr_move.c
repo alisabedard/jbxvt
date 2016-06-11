03c44cd41a22d10f6e42918d01aadba15b42ec00
Jeffrey E. Bedard <jefbed@gmail.com>
2016-06-11 03:51:42 -0400

    Use stdint types.  Simplified with shortcut pointer c.

--- scr_move.c
+++ scr_move.c
@@ -9,50 +9,38 @@
 /*  Move the cursor to a new position.  The relative argument is a pair of
  *  flags that specify relative rather than absolute motion.
  */
-void scr_move(int x, int y, int relative)
+
+void scr_move(const int16_t x, const int16_t y, const uint8_t relative)
 {
 	home_screen();
 	cursor(CURSOR_DRAW);
-	jbxvt.scr.current->cursor.x = (relative & COL_RELATIVE)
-		? jbxvt.scr.current->cursor.x + x : x;
-	reset_row_col();
-
+	xcb_point_t * restrict c = &jbxvt.scr.current->cursor;
+	c->x = (relative & COL_RELATIVE) ? c->x + x : x;
 	if (relative & ROW_RELATIVE) {
 		if (y > 0) {
-			if (jbxvt.scr.current->cursor.y
-				<= jbxvt.scr.current->margin.bottom
-				&& jbxvt.scr.current->cursor.y + y
-				> jbxvt.scr.current->margin.bottom)
-				jbxvt.scr.current->cursor.y
-					= jbxvt.scr.current->margin.bottom;
+			if (c->y <= jbxvt.scr.current->margin.bottom
+				&& c->y + y > jbxvt.scr.current->margin.bottom)
+				c->y = jbxvt.scr.current->margin.bottom;
 			else
-				jbxvt.scr.current->cursor.y += y;
+				c->y += y;
 		} else if (y < 0) {
-			if (jbxvt.scr.current->cursor.y
-				>= jbxvt.scr.current->margin.top
-				&& jbxvt.scr.current->cursor.y + y
+			if (c->y >= jbxvt.scr.current->margin.top && c->y + y
 				< jbxvt.scr.current->margin.top)
-				jbxvt.scr.current->cursor.y
-					= jbxvt.scr.current->margin.top;
+				c->y = jbxvt.scr.current->margin.top;
 			else
-				jbxvt.scr.current->cursor.y += y;
+				c->y += y;
 		}
 	} else {
 		if (jbxvt.scr.current->decom) {
-			jbxvt.scr.current->cursor.y = y
-				+ jbxvt.scr.current->margin.top;
-			if (jbxvt.scr.current->cursor.y
-				> jbxvt.scr.current->margin.bottom)
-				jbxvt.scr.current->cursor.y
-					= jbxvt.scr.current->margin.bottom;
+			c->y = y + jbxvt.scr.current->margin.top;
+			if (c->y > jbxvt.scr.current->margin.bottom)
+				c->y = jbxvt.scr.current->margin.bottom;
 		} else
-			jbxvt.scr.current->cursor.y = y;
+			c->y = y;
 	}
 	reset_row_col();
-
 	jbxvt.scr.current->wrap_next = 0;
-	check_selection(jbxvt.scr.current->cursor.y,
-		jbxvt.scr.current->cursor.y);
+	check_selection(c->y, c->y);
 	cursor(CURSOR_DRAW);
 }
 
