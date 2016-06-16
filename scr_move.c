b9bc5782fc346ccf371cf20502e5b7a3d02abfb0
Jeffrey E. Bedard <jefbed@gmail.com>
2016-06-16 03:27:21 -0400

    Remove dead code.  Use abs.

--- scr_move.c
+++ scr_move.c
@@ -21,20 +21,11 @@ void scr_move(const int16_t x, const int16_t y, const uint8_t relative)
 	cursor(CURSOR_DRAW); // clear
 	xcb_point_t * restrict c = &jbxvt.scr.current->cursor;
 	/* Sanitize non-relative arguments--must be positive.  */
-	c->x = relative & COL_RELATIVE ? c->x + x : ((x>=0)?x:0);
-	c->y = relative & ROW_RELATIVE ? c->y + y : ((y>=0)?y:0);
+	c->x = relative & COL_RELATIVE ? c->x + x : abs(x);
+	c->y = relative & ROW_RELATIVE ? c->y + y : abs(y);
 	reset_row_col();
 	jbxvt.scr.current->wrap_next = 0;
 	check_selection(c->y, c->y);
 	cursor(CURSOR_DRAW); // draw
-#if 0
-	Size m = jbxvt.scr.current->margin;
-	LOG("c->y: %d, m.bottom: %d, m.top: %d", c->y, m.b, m.t);
-	if(c->y == m.bottom - 1) {
-		int16_t d = relative & ROW_RELATIVE ? y : abs(c->y - y);
-		d = d ? d : 1;
-		scroll(m.top, m.bot, d);
-	}
-#endif
 }
 
