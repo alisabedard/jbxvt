8cfaa52bda9a0f4b60e3cf194504b34e34c15b3c
Jeffrey E. Bedard <jefbed@gmail.com>
2016-06-17 01:26:43 -0400

    Revert value checks, fixes top.

--- scr_move.c
+++ scr_move.c
@@ -21,8 +21,8 @@ void scr_move(const int16_t x, const int16_t y, const uint8_t relative)
 	cursor(CURSOR_DRAW); // clear
 	xcb_point_t * restrict c = &jbxvt.scr.current->cursor;
 	/* Sanitize non-relative arguments--must be positive.  */
-	c->x = relative & COL_RELATIVE ? c->x + x : abs(x);
-	c->y = relative & ROW_RELATIVE ? c->y + y : abs(y);
+	c->x = relative & COL_RELATIVE ? c->x + x : ((x>=0)?x:0);
+	c->y = relative & ROW_RELATIVE ? c->y + y : ((y>=0)?y:0);
 	reset_row_col();
 	jbxvt.scr.current->wrap_next = 0;
 	check_selection(c->y, c->y);
