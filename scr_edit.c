f7488e317e5f57640cb7ff9cf7f7d43dc7910e54
Jeffrey E. Bedard <jefbed@gmail.com>
2016-06-23 05:40:28 -0400

    Combine scr_delete_characters and scr_insert_characters into scr_edit module.

--- scr_edit.c
+++ scr_edit.c
@@ -1,7 +1,7 @@
 /*  Copyright 2016, Jeffrey E. Bedard
     Copyright 1992, 1997 John Bovey, University of Kent at Canterbury.*/
 
-#include "scr_delete_characters.h"
+#include "scr_edit.h"
 
 #include "config.h"
 #include "cursor.h"
@@ -11,6 +11,40 @@
 #include "selection.h"
 
 #include <string.h>
+
+//  Insert count spaces from the current position.
+void scr_insert_characters(int8_t count)
+{
+	LOG("scr_insert_characters(%d)", count);
+	count = MAX(count, 0);
+	const uint8_t cw = jbxvt.scr.chars.width;
+	count = MIN(count, cw);
+	home_screen();
+	cursor(CURSOR_DRAW);
+	VTScreen * restrict scr = jbxvt.scr.current;
+	const xcb_point_t c = scr->cursor;
+	check_selection(c.y, c.y);
+	uint8_t * s = scr->text[c.y];
+	uint32_t * r = scr->rend[c.y];
+	for (int16_t i = cw - 1; i >= c.x + count; --i) {
+		s[i] = s[i - count];
+		r[i] = r[i - count];
+	}
+	const Size f = { .w = jbxvt.X.font_width, .h = jbxvt.X.font_height};
+	const xcb_point_t p = { .x = MARGIN + c.x * f.width,
+		.y = MARGIN + c.y * f.height };
+	const uint16_t width = (cw - count - c.x) * f.width;
+	if (width > 0) {
+		  xcb_copy_area(jbxvt.X.xcb, jbxvt.X.win.vt, jbxvt.X.win.vt,
+			  jbxvt.X.gc.tx, p.x, p.y, p.x + count
+			  * f.width, p.y, width,
+			  f.height);
+	}
+	xcb_clear_area(jbxvt.X.xcb, 0, jbxvt.X.win.vt, p.x, p.y,
+		count * f.width, f.height);
+	scr->wrap_next = 0;
+	cursor(CURSOR_DRAW);
+}
 
 static void copy_area(const int16_t * restrict x, const int16_t y,
 	const uint16_t width)
--- scr_edit.h
+++ scr_edit.h
@@ -1,12 +1,15 @@
 /*  Copyright 2016, Jeffrey E. Bedard
     Copyright 1992, 1997 John Bovey, University of Kent at Canterbury.*/
 
-#ifndef SCR_DEL_CHARS_H
-#define SCR_DEL_CHARS_H
+#ifndef SCR_EDIT_H
+#define SCR_EDIT_H
 
 #include <stdint.h>
+
+//  Insert count spaces from the current position.
+void scr_insert_characters(int8_t count);
 
 //  Delete count characters from the current position.
 void scr_delete_characters(uint8_t count);
 
-#endif//SCR_DEL_CHARS_H
+#endif//!SCR_EDIT_H
