2bf9cb7c05cc2bc12d2f1818a0936f946a051186
Jeffrey E. Bedard <jefbed@gmail.com>
2016-06-23 06:18:39 -0400

    Resolved includes.

--- ttyinit.c
+++ ttyinit.c
@@ -33,18 +33,12 @@
 
 #include <fcntl.h>
 #include <grp.h>
-#include <pwd.h>
 #include <signal.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/syscall.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-#include <termios.h>
-#include <time.h>
-#include <unistd.h>
 
 /*  NOTES ON PORTING
  *
