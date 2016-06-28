8a1eda1c36ce3cbc493a38b21fb95600b93448ce
Jeffrey E. Bedard <jefbed@gmail.com>
2016-06-28 03:15:28 -0400

    Convert defines to an enum.

--- token.h
+++ token.h
@@ -31,87 +31,84 @@
  */
 enum { MAINWIN, SCREEN, SCROLLBAR };
 
-/*  Token types
- */
-#define TK_NULL		0	/* null token to be ignored */
-#define TK_STRING	1	/* string of printable characters */
-#define TK_CHAR		2	/* single character */
-#define TK_EOF		3	/* read end of file */
+//  Token types
+enum {
+TK_NULL = 0, // null token to be ignored
+	TK_STRING =  1, // string of printable characters
+	TK_CHAR =  2, // single character
+	TK_EOF = 3, // read end of file
+	TK_ENTRY = 4, // cursor crossed window boundery
+	TK_EXPOSE = 5, // window has been exposed
+	TK_RESIZE = 6, // main window has been resized
+	TK_SBSWITCH = 7, // switch scrollbar in or out
+	TK_SBGOTO = 8, // scrollbar goto
+	TK_SBUP = 9, // scrollbar move up
 
-#define TK_ENTRY	4	/* cursor crossed window boundery */
-#define TK_EXPOSE	5	/* window has been exposed */
-#define TK_RESIZE	6	/* main window has been resized */
+	TK_SBDOWN = 10, // scrollbar move down
+	TK_SELSTART = 11, // start the selection
+	TK_SELEXTND = 12, // extend the selection
+	TK_SELDRAG = 13, // drag the selection
+	TK_SELINSRT = 14, // insert the selection
+	TK_SELWORD = 15, // select a word
+	TK_SELLINE = 16, // select a line
+	TK_SELECT = 17, // confirm the selection
+	TK_SELCLEAR = 18, // selection clear request
+	TK_SELNOTIFY = 19, // selection notify request
 
-#define TK_SBSWITCH	7	/* switch scrollbar in or out */
-#define TK_SBGOTO	8	/* scrollbar goto */
-#define TK_SBUP		9	/* scrollbar move up */
-#define TK_SBDOWN	10	/* scrollbar move down */
+	TK_SELREQUEST = 20, // selection request
+	TK_TXTPAR = 21, // seq with text parameter
+	TK_FOCUS = 22, // keyboard focus event
 
-#define TK_SELSTART	11	/* start the selection */
-#define TK_SELEXTND	12	/* extend the selection */
-#define TK_SELDRAG	13	/* drag the selection */
-#define TK_SELINSRT	14	/* insert the selection */
-#define TK_SELWORD	15	/* select a word */
-#define TK_SELLINE	16	/* select a line */
-#define TK_SELECT	17	/* confirm the selection */
+	// DEC VT100 control sequence token types
+	// Tokens > 1000 are artificial.
+	TK_RIS = 1000, // reset to initial state
+	TK_ENTGM52 = 1001, // enter vt52 graphics mode (ESC F)
+	TK_EXTGM52 = 1002, // exit vt52 graphics mode (ESC G);
 
-#define TK_SELCLEAR	18	/* selection clear request */
-#define TK_SELNOTIFY	19	/* selection notify request */
-#define TK_SELREQUEST	20	/* selection request */
+	TK_CUU = 'A', // Cursor up
+	TK_CUD = 'B', // cursor down
+	TK_CUF = 'C', // cursor back
+	TK_CUB = 'D', // cursor back
+	TK_CHA = 'G', // cursor CHaracter Absolute [column]
+	TK_CUP = 'H', // position cursor
 
-#define TK_TXTPAR	21	/* seq with text parameter */
+	TK_ED = 'J', // erase to start or end of screen
+	TK_EL = 'K', // erase to start or end of line
+	TK_IL = 'L', // insert lines
+	TK_DL = 'M', // delete lines
+	TK_DCH = 'P', // Delete characters
+	TK_ECH = 'X', // Erase CHaracters
+	TK_ICH = '@', // insert characters
+	TK_DA = 'c', // device attributes request
+	TK_VPA = 'd', // vertical position absolute
+	TK_VPR = 'e', // vertical position relative
+	TK_HVP = 'f', // horizontal and vertical position
+	TK_TBC = 'g', // tab clear
+	TK_SU = 'S', // Scroll Up # lines
+	TK_SD = 'T', // Scroll Down # lines
+	TK_SET = 'h', // set mode
+	TK_RESET = 'l', // reset mode
+	TK_SGR = 'm', // set graphics rendition
+	TK_DSR = 'n', // report status or position
+	TK_DECSTBM = 'r', // set top and bottom margins
+        // ^-- also restore DEC mode values
+	TK_DECSAVEPM = 's', // Save DEC (re)set mode values
 
-#define TK_FOCUS	22	/* keyboard focus event */
+	TK_DECSWH = '#', // set character width or height
+	TK_SCS0 = '(', // set character set G0
+	TK_SCS1 = ')', // set character set G1
+	TK_DECSC = '7', // save cursor position
+	TK_DECRC = '8', // restore cursor position
+	TK_DECPAM = '=', // keypad to applications mode
+	TK_DECPNM = '>', // keypad to numeric mode
 
-/*  DEC VT100 control sequence token types */
-// Tokens >= 1000 are artificial.
-#define TK_RIS		1000 // reset to initial state
-#define TK_ENTGM52	1001 // enter vt52 graphics mode (ESC F)
-#define TK_EXTGM52	1002 // exit vt52 graphics mode (ESC G);
-
-#define TK_CUU		'A'	/* Cursor up */
-#define TK_CUD		'B'	/* cursor down */
-#define TK_CUF		'C'	/* cursor back */
-#define TK_CUB		'D'	/* cursor back */
-#define TK_CHA		'G'	// cursor CHaracter Absolute [column]
-#define TK_CUP		'H'	/* position cursor */
-
-#define TK_ED		'J'	/* erase to start or end of screen */
-#define TK_EL		'K'	/* erase to start or end of line */
-#define TK_IL		'L'	/* insert lines */
-#define TK_DL		'M'	/* delete lines */
-#define TK_DCH		'P'	/* Delete characters */
-#define TK_ECH		'X'	// Erase CHaracters
-#define TK_ICH		'@'	/* insert characters */
-#define TK_DA		'c'	/* device attributes request */
-#define TK_VPA		'd'	// vertical position absolute
-#define TK_VPR		'e'	// vertical position relative
-#define TK_HVP		'f'	/* horizontal and vertical position */
-#define TK_TBC		'g'	/* tab clear */
-#define TK_SU		'S'	// Scroll Up # lines
-#define TK_SD		'T'	// Scroll Down # lines
-#define TK_SET		'h'	/* set mode */
-#define TK_RESET	'l'	/* reset mode */
-#define TK_SGR		'm'	/* set graphics rendition */
-#define TK_DSR		'n'	/* report status or position */
-#define TK_DECSTBM	'r'	/* set top and bottom margins */
-				// ^-- also restore DEC mode values
-#define TK_DECSAVEPM	's'	// Save DEC (re)set mode values
-
-#define TK_DECSWH	'#'	/* set character width or height */
-#define TK_SCS0		'('	/* set character set G0 */
-#define TK_SCS1		')'	/* set character set G1 */
-#define TK_DECSC	'7'	/* save cursor position */
-#define TK_DECRC	'8'	/* restore cursor position */
-#define TK_DECPAM	'='	/* keypad to applications mode */
-#define TK_DECPNM	'>'	/* keypad to numeric mode */
-
-#define TK_IND		0x100	/* index downward */
-#define TK_NEL		0x101	/* beginning of next line */
-#define TK_HTS		0x102	/* horizontal tab set */
-#define TK_RI		0x103	/* reverse index */
-#define TK_SS2		0x104	/* single shift 2 */
-#define TK_SS3		0x105	/* single shift 3 */
-#define TK_DECID	0x106	/* request terminal ID */
+	TK_IND = 0x100, // index downward
+	TK_NEL = 0x101, // beginning of next line
+	TK_HTS = 0x102, // horizontal tab set
+	TK_RI = 0x103, // reverse index
+	TK_SS2 = 0x104, // single shift 2
+	TK_SS3 = 0x105, // single shift 3
+	TK_DECID = 0x106, // request terminal ID
+};
 
 #endif//!TOKEN_H
