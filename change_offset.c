859239834a7f6c83b813c198f855eef06076cc3f
Jeffrey E. Bedard <jefbed@gmail.com>
2016-07-03 13:46:29 -0400

    Simplified greatly to ease scroll debugging.

--- change_offset.c
+++ change_offset.c
@@ -3,57 +3,23 @@
 
 #include "change_offset.h"
 
-#include "config.h"
 #include "cursor.h"
 #include "jbxvt.h"
-#include "log.h"
 #include "repaint.h"
 #include "sbar.h"
-#include "screen.h"
-
-// Text has moved down by less than a screen, render lines that remain
-static void copy_repaint(const int16_t d, const int16_t y1,
-	const int16_t y2, const int row1, const int row2)
-{
-	const uint16_t height = (jbxvt.scr.chars.height - d)
-		* jbxvt.X.font_size.height;
-	xcb_copy_area(jbxvt.X.xcb, jbxvt.X.win.vt, jbxvt.X.win.vt,
-		jbxvt.X.gc.tx, 0, y1, 0, y2, jbxvt.scr.pixels.width,
-		height);
-	repaint((xcb_point_t){.y=row1}, (xcb_point_t){.y=row2,
-		.x=jbxvt.scr.chars.width-1});
-}
 
 //  Change the value of the scrolled screen offset and repaint the screen
 void change_offset(int16_t n)
 {
-	if (n > jbxvt.scr.sline.top)
-		n = jbxvt.scr.sline.top;
-	if (n < 0)
-		n = 0;
+	const int32_t t = jbxvt.scr.sline.top;
+	n = MIN(MAX(n, 0), t);
 	if (n == jbxvt.scr.offset)
 		return;
-	cursor(CURSOR_DRAW);
-	int16_t d = n - jbxvt.scr.offset;
 	jbxvt.scr.offset = n;
-	if (likely(d > 0 && d < jbxvt.scr.chars.height)) {
-		/*  Text has moved down by less than a screen so raster
-		 *  the lines that did not move off.  */
-		copy_repaint(d, MARGIN,
-			MARGIN + d * jbxvt.X.font_size.height,
-			0, d - 1);
-	} else if (d < 0 && -d < jbxvt.scr.chars.height) {
-		/*  Text has moved down by less than a screen so raster
-		 *  the lines that did not move off.  */
-		d = -d;
-		copy_repaint(d, MARGIN + d * jbxvt.X.font_size.height,
-			MARGIN, jbxvt.scr.chars.height - d,
-			jbxvt.scr.chars.height - 1);
-	}
-	cursor(CURSOR_DRAW);
-	// Update current scrollbar position due to change
-	sbar_show(jbxvt.scr.chars.height + jbxvt.scr.sline.top - 1,
-		jbxvt.scr.offset, jbxvt.scr.offset
-		+ jbxvt.scr.chars.height - 1);
+	const Size c = jbxvt.scr.chars;
+	cursor(CURSOR_DRAW); // clear
+	repaint((xcb_point_t){}, (xcb_point_t){c.w, c.h});
+	cursor(CURSOR_DRAW); // draw
+	sbar_show(c.h + t - 1, n, n + c.h - 1);
 }
 
