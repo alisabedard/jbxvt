2d0e56709f8bb65d9486b5381aa97aa557c95023
Jeffrey E. Bedard <jefbed@gmail.com>
2016-07-04 00:50:32 -0400

    Eliminate divide by zero.

--- scr_request_selection.c
+++ scr_request_selection.c
@@ -13,28 +13,23 @@
 //  Send the selection to the command after converting LF to CR.
 static void send_selection(uint8_t * str, const uint16_t count)
 {
-	for (uint16_t i = 0; i < count; i++)
+	for (uint16_t i = count; i >= 0; --i)
 		if (str[i] == '\n')
 			str[i] = '\r';
-	send_string(str,count);
+	send_string(str, count);
 }
 
 void request_selection(void)
 {
-	unsigned long nread = 0;//, bytes_after;
-	uint8_t * data;
-	// divide by 4 to convert 32 bit words to bytes
 	xcb_get_property_cookie_t c = xcb_get_property(jbxvt.X.xcb,
 		false, jbxvt.X.screen->root, XCB_ATOM_CUT_BUFFER0,
-		XCB_ATOM_STRING, nread / 4, PROP_SIZE);
+		XCB_ATOM_STRING, 0, PROP_SIZE);
 	xcb_get_property_reply_t * r = xcb_get_property_reply(
 		jbxvt.X.xcb, c, NULL);
 	if (!r)
 		  return;
-	data = xcb_get_property_value(r);
-	nread += xcb_get_property_value_length(r);
-	//bytes_after = r->bytes_after;
-	send_selection(data, xcb_get_property_value_length(r));
+	send_selection(xcb_get_property_value(r),
+		xcb_get_property_value_length(r));
 	free(r);
 }
 
