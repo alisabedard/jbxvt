7fd2ed0ecd1591c0c5f2c3cd18058465fbfd32af
Jeffrey E. Bedard <jefbed@gmail.com>
2016-07-04 18:15:22 -0400

    Fixed selections, reverted change.

--- save_selection.c
+++ save_selection.c
@@ -13,18 +13,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-static uint8_t * common(uint8_t * restrict str, uint8_t * restrict source,
-	uint16_t * restrict total, int16_t * restrict col1, const uint16_t len)
-{
-	str = GC_REALLOC(str, *total + len);
-	if(!str)
-		  abort();
-	strncpy((char *)str + *total - 1, (char *)source, len);
-	total += len;
-	return str;
-	*col1 = 0;
-}
-
 /*  Convert the currently marked screen selection as a text string
     and save it as the current saved selection. */
 void save_selection(void)
@@ -42,7 +30,6 @@ void save_selection(void)
 	se2 = forward ? &jbxvt.sel.end2 : &jbxvt.sel.end1;
 	total = 1;
 	str = GC_MALLOC(1);
-	const uint8_t w = jbxvt.scr.chars.width;
 	if (se1->se_type == SAVEDSEL) {
 		col1 = se1->se_col;
 		for (i = se1->se_index; i >= 0; --i) {
@@ -51,10 +38,15 @@ void save_selection(void)
 				col2 = se2->se_col - 1;
 				i = 0;	// force loop exit
 			} else
-				col2 = w - 1;
+				col2 = jbxvt.scr.chars.width - 1;
 			len = sl->sl_length;
-			s = convert_line(sl->sl_text, &len, col1, col2);
-			str = common(str, s, &total, &col1, len);
+			s = convert_line(sl->sl_text,&len,col1,col2);
+			str = GC_REALLOC(str,total + len);
+			if (str == NULL)
+				abort();
+			strncpy((char *)str + total - 1,(char *)s,len);
+			total += len;
+			col1 = 0;
 		}
 	}
 	if (se2->se_type == SCREENSEL) {
@@ -62,13 +54,18 @@ void save_selection(void)
 		i = is_screensel ? se1->se_index : 0;
 		col1 = is_screensel ? se1->se_col : 0;
 		for (; i <= se2->se_index; ++i) {
-			col2 = i == se2->se_index ? se2->se_col : w;
+			col2 = i == se2->se_index ? se2->se_col
+				: jbxvt.scr.chars.width;
 			if (--col2 < 0)
 				break;
-			len = w;
-			s = convert_line(jbxvt.scr.current->text[i], &len,
-				col1, col2);
-			common(str, s, &total, &col1, len);
+			len = jbxvt.scr.chars.width;
+			s = convert_line(jbxvt.scr.current->text[i],&len,col1,col2);
+			str = GC_REALLOC(str,total + len);
+			if(!str)
+				  abort();
+			strncpy((char *)str + total - 1,(char *)s,len);
+			total += len;
+			col1 = 0;
 		}
 	}
 	str[total - 1] = 0; // null termination
