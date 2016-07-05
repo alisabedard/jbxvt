a853662639adfe88fe07bb7c6d5e63c49e1c215f
Jeffrey E. Bedard <jefbed@gmail.com>
2016-07-05 01:17:25 -0400

    Use xcb types where appropriate.

--- xeventst.h
+++ xeventst.h
@@ -8,16 +8,16 @@
 typedef struct xeventst {
 	struct xeventst *next;
 	struct xeventst *prev;
-	uint32_t time;
-	uint32_t requestor; // selections
-	uint32_t window;
-	uint32_t property; // selections
-	uint32_t target;
-	uint32_t detail;
 	uint32_t type;
+	xcb_timestamp_t time;
+	xcb_window_t window;
+	xcb_window_t requestor; // selections
+	xcb_atom_t property; // selections
+	xcb_atom_t target;
+	uint32_t detail;
 	uint32_t button;
-	uint32_t state;
 	xcb_rectangle_t box;
+	uint16_t state;
 } JBXVTEvent;
 
 #endif//!XEVENTST_H
