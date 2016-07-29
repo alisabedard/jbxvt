3c082b37ab93431d8a9b21e08b6b6bb58bb40e69
Jeffrey E. Bedard <jefbed@gmail.com>
2016-07-29 01:53:07 -0400

    Remove prototypes to functions which no longer exist.

--- screen.h
+++ screen.h
@@ -27,11 +27,4 @@ void scr_efill(void);
 // Scroll from top to current bottom margin count lines, moving cursor
 void scr_index_from(const int8_t count, const int16_t top);
 
-/*  Move the display so that line represented by scrollbar value y is at the top
- *  of the screen.  */
-void scr_move_to(int16_t y);
-
-//  Send the name of the current display to the command.
-void scr_report_display(void);
-
 #endif//!SCREEN_H
