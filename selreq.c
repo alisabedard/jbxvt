bed480e026d134ca77f4904ca2549635781cc558
Jeffrey E. Bedard <jefbed@gmail.com>
2016-08-05 01:18:33 -0400

    Removed obsolete cut buffer support.  Paste the selection directly if we own it.  Remove check of SECONDARY selection.

--- selreq.c
+++ selreq.c
@@ -15,23 +15,27 @@
 #include <unistd.h>
 
 //  Send the selection to the command
-static void send_selection(uint8_t * str, const uint16_t count)
+static void send_selection(uint8_t * str, const uint16_t sz)
 {
-	jb_check(write(jbxvt.com.fd, str, count) != -1,
+	jb_check(write(jbxvt.com.fd, str, sz) != -1,
 		"Could not write to command");
 }
 
-static void paste_root(void)
+/* If we own the selection, skip the X interaction and paste directly.  */
+static bool we_own_selection(void)
 {
-	LOG("using root window XCB_ATOM_CUT_BUFFER0");
-	xcb_get_property_cookie_t c = xcb_get_property(jbxvt.X.xcb, false,
-		jbxvt.X.screen->root, XCB_ATOM_CUT_BUFFER0,
-		XCB_ATOM_STRING, 0, JBXVT_PROP_SIZE);
-	xcb_get_property_reply_t * r = xcb_get_property_reply(
-		jbxvt.X.xcb, c, NULL);
-	send_selection(xcb_get_property_value(r),
-		xcb_get_property_value_length(r));
+	struct JBXVTXData * X = &jbxvt.X;
+	const xcb_get_selection_owner_cookie_t c
+		= xcb_get_selection_owner(X->xcb, XCB_ATOM_PRIMARY);
+	xcb_get_selection_owner_reply_t * r
+		= xcb_get_selection_owner_reply(X->xcb, c, NULL);
+	if (!r)
+		return false;
+	const bool ret = r->owner == X->win.vt;
 	free(r);
+	if (ret)
+		send_selection(jbxvt.sel.text, jbxvt.sel.length);
+	return ret;
 }
 
 static bool paste_from(const xcb_atom_t clipboard, const xcb_timestamp_t t)
@@ -53,9 +57,12 @@ static bool paste_from(const xcb_atom_t clipboard, const xcb_timestamp_t t)
 		jbxvt.X.win.vt, XCB_ATOM_STRING, XCB_ATOM_ANY, 0,
 		JBXVT_PROP_SIZE);
 	xcb_get_property_reply_t * p_r = xcb_get_property_reply(x, p_c, NULL);
-	if (!p_r)
-		  return false;
+	if (!p_r) {
+		LOG("Could not get property");
+		return false;
+	}
 	if (p_r->type == XCB_ATOM_NONE) {
+		LOG("XCB_ATOM_NONE");
 		free(p_r);
 		return false;
 	}
@@ -67,13 +74,12 @@ static bool paste_from(const xcb_atom_t clipboard, const xcb_timestamp_t t)
 
 void request_selection(const xcb_timestamp_t t)
 {
+	if (we_own_selection())
+		return;
 	if (paste_from(XCB_ATOM_PRIMARY, t))
-		  return;
+		return;
 	if (paste_from(jbxvt.X.clipboard, t))
-		  return;
-	if (paste_from(XCB_ATOM_SECONDARY, t))
-		  return;
-	paste_root();
+		return;
 }
 
 //  Respond to a notification that a primary selection has been sent
