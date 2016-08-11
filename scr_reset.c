7fbe7816b1dcaa0d01aa951e60d11052b53c9531
Jeffrey E. Bedard <jefbed@gmail.com>
2016-08-11 03:21:34 -0400

    Working implementation of DECSCNM (reverse video).  Use preprocessor shortcuts.  Resolved includes.

--- scr_reset.c
+++ scr_reset.c
@@ -3,27 +3,27 @@
 
 #include "scr_reset.h"
 
-#include "command.h"
 #include "config.h"
 #include "cursor.h"
 #include "jbxvt.h"
 #include "libjb/log.h"
-#include "libjb/util.h"
 #include "repaint.h"
 #include "sbar.h"
-#include "screen.h"
-#include "scroll.h"
 #include "scr_move.h"
-#include "selection.h"
+#include "scroll.h"
 
 #include <gc.h>
-#include <string.h>
 #include <unistd.h>
+
+// Shortcuts
+#define S jbxvt.scr
+#define P S.pixels
+#define X jbxvt.X
 
 static void init_screen_elements(VTScreen * restrict scr,
 	uint8_t ** restrict text, uint32_t ** restrict rend)
 {
-	scr->margin.bottom = jbxvt.scr.chars.height - 1;
+	scr->margin.bottom = S.chars.height - 1;
 	scr->wrap_next = false;
 	scr->rend = rend;
 	scr->text = text;
@@ -33,7 +33,7 @@ static void init_screen_elements(VTScreen * restrict scr,
 __attribute__((pure))
 static Size get_cdim(const Size d)
 {
-	const Size f = jbxvt.X.f.size;
+	const Size f = X.f.size;
 	// Take advantage of division rounding down for mod
 	return (Size) {.w = (d.w - MARGIN)/f.w,
 		.h = (d.h - MARGIN)/f.h};
@@ -44,55 +44,39 @@ static void init(void)
 	uint8_t **s1, **s2;
 	uint32_t **r1, **r2;
 	uint16_t sz = JBXVT_MAX_ROWS * sizeof(void *);
-	s1 = GC_MALLOC(sz);
-	s2 = GC_MALLOC(sz);
-	r1 = GC_MALLOC(sz);
-	r2 = GC_MALLOC(sz);
+#define ALLOC(v) {v = GC_MALLOC(sz);}
+	ALLOC(s1); ALLOC(s2); ALLOC(r1); ALLOC(r2);
 	for (int_fast16_t y = JBXVT_MAX_ROWS - 1; y >= 0; --y) {
 		sz = JBXVT_MAX_COLS;
-		s1[y] = GC_MALLOC(sz);
-		s2[y] = GC_MALLOC(sz);
+		ALLOC(s1[y]); ALLOC(s2[y]);
 		sz <<= 2;
-		r1[y] = GC_MALLOC(sz);
-		r2[y] = GC_MALLOC(sz);
+		ALLOC(r1[y]); ALLOC(r2[y]);
 	}
-	jbxvt.scr.s[0].text = s1;
-	jbxvt.scr.s[1].text = s2;
-	jbxvt.scr.s[0].rend = r1;
-	jbxvt.scr.s[1].rend = r2;
-	jbxvt.scr.s[0].wrap = GC_MALLOC(JBXVT_MAX_ROWS);
-	jbxvt.scr.s[1].wrap = GC_MALLOC(JBXVT_MAX_ROWS);
+	S.s[0].text = s1;
+	S.s[1].text = s2;
+	S.s[0].rend = r1;
+	S.s[1].rend = r2;
+	sz = JBXVT_MAX_ROWS;
+	ALLOC(S.s[0].wrap)
+	ALLOC(S.s[1].wrap)
 }
 
 static inline void fix_margins(const Size c)
 {
 	/* On screen resize, check if old margin was on the bottom line.
 	   If so, set the bottom margin to the new bottom line.  */
-	if (c.height == jbxvt.scr.chars.height)
+	if (c.height == S.chars.height)
 		  return;
-	VTScreen * restrict s = jbxvt.scr.current;
+	VTScreen * restrict s = S.current;
 	if (s->margin.b >= c.h)
 		  s->margin.b = c.h - 1;
 }
-#if 0
 static void clear(void)
 {
-	struct JBXVTXData * x = &jbxvt.X;
-	struct JBXVTScreenData * scr = &jbxvt.scr;
-	const Size sz = scr->pixels;
-	xcb_clear_area(x->xcb, false, x->win.vt, 0, 0, sz.w, sz.h<<1);
-	xcb_flush(x->xcb);
+	xcb_clear_area(X.xcb, false, X.win.vt, 0, 0, P.w, P.h);
 	change_offset(0);
-	VTScreen * s = scr->current;
-	const Size c = scr->chars;
-	for (int i = s->cursor.y; i < c.height; ++i)
-		for (int j = i == s->cursor.y ? s->cursor.x : 0;
-			i < c.w; ++i) {
-			s->text[i][j] = 0;
-			s->rend[i][j] = 0;
-		}
+	xcb_flush(X.xcb);
 }
-#endif
 
 static void decscnm(void)
 {
@@ -100,15 +84,16 @@ static void decscnm(void)
 	const bool rv = jbxvt.mode.decscnm;
 	if (last_was_rv == rv) // Already has either mode set
 		return;
+	else
+		last_was_rv = rv;
 	LOG("decscnm()");
-	struct JBXVTXPixels * p = &jbxvt.X.color;
+	struct JBXVTXPixels * p = &X.color;
 	SWAP(pixel_t, p->fg, p->bg);
 	SWAP(pixel_t, p->current_fg, p->current_bg);
-	xcb_change_gc(jbxvt.X.xcb, jbxvt.X.gc.tx, XCB_GC_FOREGROUND
+	xcb_change_gc(X.xcb, X.gc.tx, XCB_GC_FOREGROUND
 		| XCB_GC_BACKGROUND, (uint32_t[]){p->fg, p->bg});
-	xcb_change_window_attributes(jbxvt.X.xcb, jbxvt.X.win.vt,
+	xcb_change_window_attributes(X.xcb, X.win.vt,
 		XCB_CW_BACK_PIXEL, &p->bg);
-	xcb_flush(jbxvt.X.xcb);
 	usleep(100000);
 }
 
@@ -118,36 +103,37 @@ void scr_reset(void)
 {
 	LOG("scr_reset()");
 	decscnm();
-	Size c = get_cdim(jbxvt.scr.pixels);
+	Size c = get_cdim(P);
 	fix_margins(c);
 	static bool created;
 	if (!created) {
 		init();
 		created = true;
 	}
-	uint8_t **s1 = jbxvt.scr.s[0].text, **s2 = jbxvt.scr.s[1].text;
-	uint32_t **r1 = jbxvt.scr.s[0].rend, **r2 = jbxvt.scr.s[1].rend;
-	VTScreen * scr = jbxvt.scr.current;
-	if (likely(scr == &jbxvt.scr.s[0] && jbxvt.scr.s[0].text)
-		&& scr->cursor.y >= c.h) {
-		scroll1(scr->cursor.y - c.h + 1);
-		scr->cursor.y = c.h - 1;
+	uint8_t **s1 = S.s[0].text, **s2 = S.s[1].text;
+	uint32_t **r1 = S.s[0].rend, **r2 = S.s[1].rend;
+	VTScreen * scr = S.current;
+	int16_t * y = &scr->cursor.y;
+	if (likely(scr == &S.s[0] && S.s[0].text) && *y >= c.h) {
+		scroll1(*y - c.h + 1);
+		*y = c.h - 1;
 	}
-	init_screen_elements(&jbxvt.scr.s[0], s1, r1);
-	init_screen_elements(&jbxvt.scr.s[1], s2, r2);
+	init_screen_elements(&S.s[0], s1, r1);
+	init_screen_elements(&S.s[1], s2, r2);
 	scr_start_selection((xcb_point_t){}, SEL_CHAR);
 	// Constrain dimensions:
 	c.w = MIN(c.w, JBXVT_MAX_COLS);
 	c.h = MIN(c.h, JBXVT_MAX_ROWS);
 	tty_set_size(c.w, c.h);
-	jbxvt.scr.chars = c;
+	S.chars = c;
 	reset_row_col();
 	--c.h; --c.w;
-	sbar_show(c.h + jbxvt.scr.sline.top, jbxvt.scr.offset,
-		jbxvt.scr.offset + c.h);
+	sbar_show(c.h + S.sline.top, S.offset,
+		S.offset + c.h);
+	clear();
 	decscnm();
+	xcb_flush(X.xcb);
 	repaint();
 	draw_cursor();
-	xcb_flush(jbxvt.X.xcb);
 }
 
