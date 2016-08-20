d6dbfda9acc49647a50219009ec68157a5446c2c
Jeffrey E. Bedard <jefbed@gmail.com>
2016-08-20 02:11:34 -0400

    Reset character set selection on screen change.  This fixes alsamixer display partially.

--- screen.c
+++ screen.c
@@ -86,6 +86,7 @@ void scr_change_screen(const bool mode_high)
 	jbxvt.scr.current = &jbxvt.scr.s[mode_high];
 	jbxvt.sel.end[1].type = NOSEL;
 	jbxvt.scr.sline.top = 0;
+	jbxvt.mode.charsel = 0; // reset on screen change
 	repaint();
 	draw_cursor();
 	scr_erase_screen(2); // ENTIRE
