423308e3462927bb6be20e1d2c07f7e19a30ac6f
Jeffrey E. Bedard <jefbed@gmail.com>
2016-08-30 00:26:32 -0400

    Use JB_LIMIT.  Reduce dereferences.

--- scr_move.c
+++ scr_move.c
@@ -10,18 +10,24 @@
 
 #include <stdlib.h>
 
+#define CSZ jbxvt.scr.chars
+#define SCR jbxvt.scr.current
+#define CUR SCR->cursor
+#define MGN SCR->margin
+
 // Sanitize cursor position, implement DECOM
 void reset_row_col(void)
 {
-	struct JBDim * c = &jbxvt.scr.current->cursor;
-#define CH jbxvt.scr.chars
-#define LIMIT(var, top, bottom) var = MAX(MIN(var, top), bottom)
-	LIMIT(c->x, CH.w - 1, 0);
-	LIMIT(c->y, CH.h - 1, 0);
+	struct JBDim * c = &CUR;
+	struct JBDim sz = CSZ;
+	--sz.w; --sz.h;
+	JB_LIMIT(c->x, sz.w, 0);
+	JB_LIMIT(c->y, sz.h, 0);
 	// Implement DECOM, DEC Origin Mode, limits
-	if (jbxvt.mode.decom)
-#define M jbxvt.scr.current->margin
-		LIMIT(c->y, M.t, M.b);
+	if (jbxvt.mode.decom) {
+		const struct JBDim m = MGN;
+		JB_LIMIT(c->y, m.t, m.b);
+	}
 }
 
 /*  Move the cursor to a new position.  The relative argument is a pair of
@@ -33,13 +39,14 @@ void scr_move(const int16_t x, const int16_t y, const uint8_t relative)
 #endif//MOVE_DEBUG
 	change_offset(0);
 	draw_cursor(); // clear
-	struct JBDim * c = &jbxvt.scr.current->cursor;
-	/* Sanitize non-relative arguments--must be positive.  */
+	struct JBDim * c = &CUR;
+	// Sanitize non-relative arguments--must be positive.
 	c->x = relative & COL_RELATIVE ? c->x + x : MAX(x, 0);
-	c->y = relative & ROW_RELATIVE ? c->y + y : MAX(y, 0);
+	const int16_t y = c->y = relative & ROW_RELATIVE
+		? c->y + y : MAX(y, 0);
 	reset_row_col();
-	jbxvt.scr.current->wrap_next = 0;
-	check_selection(c->y, c->y);
+	SCR->wrap_next = 0;
+	check_selection(y, y);
 	draw_cursor(); // draw
 }
 
