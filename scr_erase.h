a57eecc73c56982943ca53d81abf9de60bbfc2fe
Jeffrey E. Bedard <jefbed@gmail.com>
2016-10-03 04:33:16 -0400

    Added prefix.

--- scr_erase.h
+++ scr_erase.h
@@ -1,8 +1,8 @@
 /*  Copyright 2016, Jeffrey E. Bedard
     Copyright 1992, 1997 John Bovey, University of Kent at Canterbury.*/
 
-#ifndef SCR_ERASE_H
-#define SCR_ERASE_H
+#ifndef JBXVT_SCR_ERASE_H
+#define JBXVT_SCR_ERASE_H
 
 #include <stdint.h>
 
@@ -12,4 +12,4 @@ void scr_erase_line(const int8_t mode);
 //  erase part or the whole of the screen
 void scr_erase_screen(const int8_t mode);
 
-#endif//!SCR_ERASE_H
+#endif//!JBXVT_SCR_ERASE_H
