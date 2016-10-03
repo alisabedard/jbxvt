3ac53713b4641637419448f06104f04b5ba8649a
Jeffrey E. Bedard <jefbed@gmail.com>
2016-10-03 04:35:25 -0400

    Removed stale definitions.  Added prefix.

--- xsetup.h
+++ xsetup.h
@@ -1,20 +1,17 @@
 /*  Copyright 2016, Jeffrey E. Bedard
     Copyright 1992, 1997 John Bovey, University of Kent at Canterbury.*/
 
-#ifndef XSETUP_H
-#define XSETUP_H
+#ifndef JBXVT_XSETUP_H
+#define JBXVT_XSETUP_H
 
 #include <stdbool.h>
 #include <stdint.h>
-
 
 // Change window or icon name:
 void change_name(uint8_t * restrict str, const bool icon);
 
 void map_window(void);
-void resize_window(void);
-void send_auth(void);
-void switch_scrollbar(void);
-void usage(int);
 
-#endif//XSETUP_H
+void resize_window(void);
+
+#endif//JBXVT_XSETUP_H
