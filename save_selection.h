984266343b3c1da9b96e774f92cbfdcc2c9e1813
Jeffrey E. Bedard <jefbed@gmail.com>
2016-10-03 10:42:37 -0400

    Added JBXVT_ prefix to preprocessor constants.

--- save_selection.h
+++ save_selection.h
@@ -1,11 +1,11 @@
 /*  Copyright 2016, Jeffrey E. Bedard
     Copyright 1992, 1997 John Bovey, University of Kent at Canterbury.*/
 
-#ifndef SAVE_SELECTION_H
-#define SAVE_SELECTION_H
+#ifndef JBXVT_SAVE_SELECTION_H
+#define JBXVT_SAVE_SELECTION_H
 
 /*  Convert the currently marked screen selection as a text string
     and save it as the current saved selection. */
 void save_selection(void);
 
-#endif//!SAVE_SELECTION_H
+#endif//!JBXVT_SAVE_SELECTION_H
