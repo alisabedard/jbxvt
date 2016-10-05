c2442cb0894b73ac3bc0d8d25fbeb30771deddd6
Jeffrey E. Bedard <jefbed@gmail.com>
2016-10-05 03:52:56 -0400

    Combined common code in call to xcb_clear_area into clear().  Added SB shortcut.

--- sbar.c
+++ sbar.c
@@ -15,6 +15,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#undef SB
+#define SB jbxvt.X.win.sb
+
 static struct {
 	uint16_t height;
 	// most recent arguments to sbar_show:
@@ -33,6 +36,12 @@ static int16_t get_sz(const int16_t lh, const uint16_t length)
 	return sbar.height - sbar.height * lh / length;
 }
 
+static void clear(const int16_t y, const uint16_t height)
+{
+	xcb_clear_area(jbxvt.X.xcb, false, SB,
+		0, y, SBAR_WIDTH, height);
+}
+
 /*  Redraw the scrollbar to show the area from low to high,
     proportional to length.  */
 void sbar_draw(uint16_t length, const int16_t low,
@@ -44,17 +53,15 @@ void sbar_draw(uint16_t length, const int16_t low,
 	sbar.last_low = low;
 	sbar.last_high = high;
 	const int16_t top = get_sz(high, length), bot = get_sz(low, length);
-	const xcb_window_t sb = jbxvt.X.win.sb;
+	const xcb_window_t sb = SB;
 	if (top > 0)
-		xcb_clear_area(jbxvt.X.xcb, false, sb, 0, 0,
-			SBAR_WIDTH, top - 1);
+		clear(0, top - 1);
 	if (bot >= top)
 		xcb_poly_fill_rectangle(jbxvt.X.xcb, sb, jbxvt.X.gc.tx, 1,
 			&(xcb_rectangle_t){0, top, SBAR_WIDTH,
 			bot - top + 1});
 	if (bot < sbar.height)
-		xcb_clear_area(jbxvt.X.xcb, false, sb, 0, bot + 1,
-			SBAR_WIDTH, sbar.height - bot - 1);
+		clear(bot + 1, sbar.height - bot - 1);
 }
 
 //  Change the value of the scrolled screen offset and repaint the screen
@@ -88,13 +95,13 @@ static void set_vt_x(const int8_t x)
 void jbxvt_show_sbar(void)
 {
 	set_vt_x(SBAR_WIDTH);
-	xcb_map_window(jbxvt.X.xcb, jbxvt.X.win.sb);
+	xcb_map_window(jbxvt.X.xcb, SB);
 }
 
 void jbxvt_hide_sbar(void)
 {
 	set_vt_x(0);
-	xcb_unmap_window(jbxvt.X.xcb, jbxvt.X.win.sb);
+	xcb_unmap_window(jbxvt.X.xcb, SB);
 }
 
 void jbxvt_toggle_sbar(void)
