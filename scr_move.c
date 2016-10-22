00704428fd7942df14df8cb2042542c2a095b906
Jeffrey E. Bedard <jefbed@gmail.com>
2016-10-22 00:43:51 -0400

    Consolidated variables.  Pass int16_t then return instead of using pointer. Remove reset of wrap_next.  Add int16_t return of y value to reset_row_col.

--- scr_move.c
+++ scr_move.c
@@ -2,45 +2,51 @@
 #include "scr_move.h"
 #include "cursor.h"
 #include "jbxvt.h"
+#include "libjb/log.h"
 #include "libjb/util.h"
 #include "sbar.h"
+#ifndef JBXVT_SCR_MOVE_DEBUG
+#undef LOG
+#define LOG(...)
+#endif//!JBXVT_SCR_MOVE_DEBUG
 // Sanitize cursor position, implement DECOM
-static void decom(struct JBDim * restrict c)
+static int16_t decom(struct JBDim * restrict c)
 {
 	// Implement DECOM, DEC Origin Mode, limits
 	if (jbxvt.mode.decom) {
 		const struct JBDim m = jbxvt.scr.current->margin;
 		JB_LIMIT(c->y, m.top, m.bottom);
 	}
+	return c->y;
 }
-void reset_row_col(void)
+// Ensure cursor coordinates are valid per screen and decom mode
+// Returns new cursor y value
+int16_t reset_row_col(void)
 {
 	struct JBDim * c = &jbxvt.scr.current->cursor;
 	JB_LIMIT(c->x, jbxvt.scr.chars.w - 1, 0);
 	JB_LIMIT(c->y, jbxvt.scr.chars.h - 1, 0);
-	decom(c);
+	return decom(c);
 }
-static void set_dimension(int16_t * restrict cursor,
+static inline int16_t dim(const int16_t cursor,
 	const int16_t delta, const bool relative)
 {
-	*cursor = relative ? *cursor + delta : JB_MAX(delta, 0);
+	// Sanitize non-relative arguments--must be positive.
+	return relative ? cursor + delta : JB_MAX(delta, 0);
 }
 /*  Move the cursor to a new position.  The relative argument is a pair of
  *  flags that specify relative rather than absolute motion.  */
 void jbxvt_move(const int16_t x, const int16_t y, const uint8_t relative)
 {
-#ifdef MOVE_DEBUG
 	LOG("jbxvt_move(x:%d, y:%d, relative:%d)", x, y, relative);
-#endif//MOVE_DEBUG
 	jbxvt_set_scroll(0);
 	jbxvt_draw_cursor(); // clear
-	struct JBDim * c = &jbxvt.scr.current->cursor;
-	// Sanitize non-relative arguments--must be positive.
-	set_dimension(&c->x, x, relative & COL_RELATIVE);
-	set_dimension(&c->y, y, relative & ROW_RELATIVE);
-	reset_row_col();
-	const int16_t cy = c->y;
-	jbxvt.scr.current->wrap_next = 0;
-	jbxvt_check_selection(cy, cy);
+	struct JBDim c = jbxvt.scr.current->cursor;
+	jbxvt.scr.current->cursor = c
+		= (struct JBDim) { .x = dim(c.x, x, relative
+			& COL_RELATIVE),
+		.y = dim(c.y, y, relative & ROW_RELATIVE)};
+	c.y = reset_row_col();
+	jbxvt_check_selection(c.y, c.y);
 	jbxvt_draw_cursor(); // draw
 }
--- scr_move.h
+++ scr_move.h
@@ -8,6 +8,7 @@ enum ScrMoveFlags {
 /*  Move the cursor to a new position.  The relative argument is a pair of
  *  flags that specify relative rather than absolute motion.  */
 void jbxvt_move(const int16_t x, const int16_t y, const uint8_t relative);
-// Sanitize cursor position, implement DECOM
-void reset_row_col(void);
+// Ensure cursor coordinates are valid per screen and decom mode
+// Returns new cursor y value
+int16_t reset_row_col(void);
 #endif//!SCR_MOVE_H
