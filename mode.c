2c9ac54609f96b50bad36c4cc4e60fdddbca26ef
Jeffrey E. Bedard <jefbed@gmail.com>
2016-11-04 23:48:02 -0400

    Add module to store dec private modes.

---
+++ mode.c
@@ -0,0 +1,26 @@
+// Copyright 2016, Jeffrey E. Bedard
+#include "mode.h"
+#include "JBXVTPrivateModes.h"
+#include <string.h>
+static struct JBXVTPrivateModes jbxvt_saved_mode;
+// Set default values for private modes
+struct JBXVTPrivateModes * jbxvt_get_modes(void)
+{
+	// Set defaults:
+	static struct JBXVTPrivateModes m = {.decanm = true,
+		.decawm = false, .dectcem = true,
+		.charset = {CHARSET_ASCII, CHARSET_ASCII}};
+	return &m;
+}
+// Restore private modes.
+void jbxvt_restore_modes(void)
+{
+	memcpy(jbxvt_get_modes(), &jbxvt_saved_mode,
+		sizeof(struct JBXVTPrivateModes));
+}
+// Save private modes.
+void jbxvt_save_modes(void)
+{
+	memcpy(&jbxvt_saved_mode, jbxvt_get_modes(),
+		sizeof(struct JBXVTPrivateModes));
+}
---
+++ mode.h
@@ -0,0 +1,11 @@
+// Copyright 2016, Jeffrey E. Bedard
+#ifndef JBXVT_MODE_H
+#define JBXVT_MODE_H
+#include "JBXVTPrivateModes.h"
+// Get pointer to statically allocated mode data structure.
+struct JBXVTPrivateModes * jbxvt_get_modes(void);
+// Restore private modes.
+void jbxvt_restore_modes(void);
+// Save private modes.
+void jbxvt_save_modes(void);
+#endif//!JBXVT_MODE_H
