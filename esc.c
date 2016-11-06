18b56c36545bc06ff903f745d9d215988138501d
Jeffrey E. Bedard <jefbed@gmail.com>
2016-11-06 02:21:57 -0500

    Removed macros.  Added comments.  Added selectors for G1, G2 and G3.  Fixed typos.

--- esc.c
+++ esc.c
@@ -74,88 +74,183 @@ void jbxvt_esc(xcb_connection_t * xc,
 	case ' ':
 		c = jbxvt_pop_char(xc, 0);
 		switch (c) {
-#define CASE_A(ch, tok, a) case ch: tk->type = tok, tk->arg[0] = a;\
-			tk->nargs=1; break;
-#define CASE_T(ch, tok) case ch: tk->type = tok; break;
-#define CASE_M(ch, mod, v) case ch: jbxvt_get_modes()->mod = v; break;
-		CASE_T('F', JBXVT_TOKEN_S7C1T);
-		CASE_T('G', JBXVT_TOKEN_S8C1T);
-		CASE_T('L', JBXVT_TOKEN_ANSI1);
-		CASE_T('M', JBXVT_TOKEN_ANSI2);
-		CASE_T('N', JBXVT_TOKEN_ANSI3);
+		case 'F':
+			tk->type = JBXVT_TOKEN_S7C1T;
+			break;
+		case 'G':
+			tk->type = JBXVT_TOKEN_S8C1T;
+			break;
+		case 'L':
+			tk->type = JBXVT_TOKEN_ANSI1;
+			break;
+		case 'M':
+			tk->type = JBXVT_TOKEN_ANSI2;
+			break;
+		case 'N':
+			tk->type = JBXVT_TOKEN_ANSI3;
+			break;
 		}
 		break;
 	case '#':
 		c = jbxvt_pop_char(xc, 0);
 		switch(c) {
-		CASE_T('3', JBXVT_TOKEN_DHLT);
-		CASE_T('4', JBXVT_TOKEN_DHLB);
-		CASE_T('5', JBXVT_TOKEN_SWL);
-		CASE_T('6', JBXVT_TOKEN_DWL);
-		CASE_T('8', JBXVT_TOKEN_ALN);
+		case '3':
+			tk->type = JBXVT_TOKEN_DHLT;
+			break;
+		case '4':
+			tk->type = JBXVT_TOKEN_DHLB;
+			break;
+		case '5':
+			tk->type = JBXVT_TOKEN_SWL;
+			break;
+		case '6':
+			tk->type = JBXVT_TOKEN_DWL;
+			break;
+		case '8':
+			tk->type = JBXVT_TOKEN_ALN;
+			break;
 		}
 		break;
-	case '(': // G0 charset
-	CASE_A(')', c, jbxvt_pop_char(xc, 0));
+	case '(': // G0 character set
+	case ')': // G1
+	case '*': // G2
+	case '+': // G3
+	case '-': // G1
+	case '.': // G2
+	case '/': // G3
+		tk->type = c;
+		tk->arg[0] = jbxvt_pop_char(xc, 0);
+		tk->nargs = 1;
+		break;
 	case '%': // UTF charset switch
 		c = jbxvt_pop_char(xc, 0);
 		switch (c) {
-		CASE_T('@', JBXVT_TOKEN_CS_DEF);
-		CASE_T('G', JBXVT_TOKEN_CS_UTF8);
+		case '@':
+			tk->type = JBXVT_TOKEN_CS_DEF;
+			break;
+		case 'G':
+			tk->type = JBXVT_TOKEN_CS_UTF8;
+			break;
 		}
 		break;
-	CASE_T('6', JBXVT_TOKEN_RI); // BI: back index
-	CASE_T('9', JBXVT_TOKEN_IND); // FI: forward index
+	case '6': // BI: back index
+		tk->type = JBXVT_TOKEN_RI;
+		break;
+	case '9': // FI: forward index
+		tk->type = JBXVT_TOKEN_IND;
+		break;
 	case '7': // SC: save cursor
 	case '8': // RC: restore cursor
 	case '=': // PAM: keypad to application mode
 	case '>': // PNM: keypad to numeric mode
 		tk->type = c;
 		break;
-	CASE_T('^', JBXVT_TOKEN_PM); // PM: Privacy message (ended by ESC \)
-	CASE_T('\\', JBXVT_TOKEN_ST);
-	CASE_M('<', decanm, true); // exit vt52 mode
+	case '^': // PM: Privacy message (ended by ESC \)
+		tk->type = JBXVT_TOKEN_PM;
+		break;
+	case '\\': // string terminator
+		tk->type = JBXVT_TOKEN_ST;
+		break;
+	case '<': // exit vt52 mode
+		jbxvt_get_modes()->decanm = true;
+		break;
 	case 'A': // vt52 cursor up
 	case 'B': // vt52 cursor down
 	case 'C': // vt52 cursor left
 		tk->type = c;
 		break;
-	CASE_T('D', jbxvt_get_modes()->decanm ? JBXVT_TOKEN_IND
-		: JBXVT_TOKEN_CUF);
-	CASE_T('c', JBXVT_TOKEN_RIS); // Reset to Initial State
-	CASE_M('e', dectcem, true); // enable cursor (vt52 GEMDOS)
-	CASE_M('f', dectcem, false); // disable cursor (vt52 GEMDOS)
-	CASE_T('E', JBXVT_TOKEN_NEL);
-	CASE_T('F', JBXVT_TOKEN_ENTGM52); // Enter VT52 graphics mode
-	CASE_T('G', JBXVT_TOKEN_EXTGM52); // Leave VT52 graphics mode
-	CASE_T('H', jbxvt_get_modes()->decanm ? JBXVT_TOKEN_HTS
-		: JBXVT_TOKEN_HOME);
-	CASE_A('l', jbxvt_get_modes()->decanm
-		? JBXVT_TOKEN_MEMLOCK : JBXVT_TOKEN_EL, 2);
+	case 'D':
+		tk->type = jbxvt_get_modes()->decanm
+			? JBXVT_TOKEN_IND : JBXVT_TOKEN_CUF;
+		break;
+	case 'c':
+		tk->type = JBXVT_TOKEN_RIS;
+		break; // Reset to Initial State
+	case 'e': // enable cursor (vt52)
+		jbxvt_get_modes()->dectcem = true;
+		break;
+	case 'f': // disable cursor (vt52)
+		jbxvt_get_modes()->dectcem = true;
+		break;
+	case 'E':
+		tk->type = JBXVT_TOKEN_NEL;
+		break;
+	case 'F': // Enter VT52 graphics mode
+		tk->type = JBXVT_TOKEN_ENTGM52;
+		break;
+	case 'G': // Leave VT52 graphics mode
+		tk->type = JBXVT_TOKEN_EXTGM52;
+		break;
+	case 'H':
+		tk->type = jbxvt_get_modes()->decanm
+			? JBXVT_TOKEN_HTS : JBXVT_TOKEN_HOME;
+		break;
+	case 'l':
+		tk->type = jbxvt_get_modes()->decanm
+			? JBXVT_TOKEN_MEMLOCK : JBXVT_TOKEN_EL;
+		tk->arg[0] = 2;
+		tk->nargs = 1;
+		break;
 	case 'I':
 		jbxvt_index_from(xc, -1, jbxvt_get_screen()->cursor.y);
 		tk->type = JBXVT_TOKEN_CUU;
 		break;
-	CASE_A('J', JBXVT_TOKEN_EL, 1); // vt52 erase to end of line
-	CASE_T('j', JBXVT_TOKEN_SC); // save cursor (vt52g)
-	CASE_T('K', JBXVT_TOKEN_ED); // vt42 erase to end of screen
-	CASE_T('k', JBXVT_TOKEN_RC); // restore cursor (vt52g)
-	CASE_T('L', JBXVT_TOKEN_IL); // insert line (vt52)
-	CASE_T('M', jbxvt_get_modes()->decanm
-		? JBXVT_TOKEN_RI : JBXVT_TOKEN_DL);
-	CASE_T('N', JBXVT_TOKEN_SS2);
-	CASE_T('O', JBXVT_TOKEN_SS3);
-	CASE_A('o', JBXVT_TOKEN_EL, 1); // clear to start of line (vt52g)
-	case 'P':
+	case 'J': // erase to end of line
+		tk->type = JBXVT_TOKEN_EL;
+		tk->arg[0] = 1;
+		tk->nargs = 1;
+		break;
+	case 'j': // save cursor
+		tk->type = JBXVT_TOKEN_SC;
+		break;
+	case 'K': // erase to end of screen
+		tk->type = JBXVT_TOKEN_ED;
+		break;
+	case 'k': // restore cursor
+		tk->type = JBXVT_TOKEN_RC;
+		break;
+	case 'L': // insert line
+		tk->type = JBXVT_TOKEN_IL;
+		break;
+	case 'M': // reverse index or delete line
+		tk->type = jbxvt_get_modes()->decanm
+			? JBXVT_TOKEN_RI : JBXVT_TOKEN_DL;
+		break;
+	case 'N':
+		tk->type = JBXVT_TOKEN_SS2;
+		break;
+	case 'O':
+		tk->type = JBXVT_TOKEN_SS3;
+		break;
+	case 'o': // clear to start of line
+		tk->type = JBXVT_TOKEN_EL;
+		tk->arg[0] = 1;
+		tk->nargs = 1;
+		break;
+	case 'P': // device control string
 		jbxvt_dcs(xc, tk);
 		break;
-	CASE_M('p', decscnm, true); // reverse video mode (vt52g)
-	CASE_M('q', decscnm, false); // normal video (vt52g)
-	CASE_T('V', JBXVT_TOKEN_SPA);
-	CASE_M('v', decawm, true); // wrap on
-	CASE_T('W', JBXVT_TOKEN_EPA);
-	CASE_M('w', decawm, false); // wrap off
-	CASE_T('X', JBXVT_TOKEN_SOS);
+	case 'p': // reverse video mode
+		jbxvt_get_modes()->decscnm = true;
+		break;
+	case 'q': // normal video mode
+		jbxvt_get_modes()->decscnm = false;
+		break;
+	case 'V': // start protected area
+		tk->type = JBXVT_TOKEN_SPA;
+		break;
+	case 'v': // auto wrap mode on
+		jbxvt_get_modes()->decawm = true;
+		break;
+	case 'W': // end protected area
+		tk->type = JBXVT_TOKEN_EPA;
+		break;
+	case 'w': // auto wrap mode off
+		jbxvt_get_modes()->decawm = false;
+		break;
+	case 'X': // start of string
+		tk->type = JBXVT_TOKEN_SOS;
+		break;
 	case 'Y':
 		tk->type = JBXVT_TOKEN_CUP;
 		// -32 to decode, + 1 to be vt100 compatible
