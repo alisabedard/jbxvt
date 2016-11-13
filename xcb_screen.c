c271ae5275fbf325b29629d71365531b9129b6ed
Jeffrey E. Bedard <jefbed@gmail.com>
2016-11-13 00:09:16 -0500

    Cache accessor values staticly.

--- xcb_screen.c
+++ xcb_screen.c
@@ -1,20 +1,18 @@
 // Copyright 2016, Jeffrey E. Bedard
 #include "xcb_screen.h"
 #include "libjb/xcb.h"
-static xcb_screen_t * jbxvt_xcb_screen;
-static void init(xcb_connection_t * restrict xc)
+static xcb_screen_t * get_xcb_screen(xcb_connection_t * restrict xc)
 {
-	jbxvt_xcb_screen = jb_get_xcb_screen(xc);
+	static xcb_screen_t * s;
+	return s ? s : (s = jb_get_xcb_screen(xc));
 }
 xcb_window_t jbxvt_get_root_window(xcb_connection_t * xc)
 {
-	if (!jbxvt_xcb_screen)
-		init(xc);
-	return jbxvt_xcb_screen->root;
+	static xcb_window_t w;
+	return w ? w : (w = get_xcb_screen(xc)->root);
 }
 xcb_colormap_t jbxvt_get_colormap(xcb_connection_t * xc)
 {
-	if (!jbxvt_xcb_screen)
-		init(xc);
-	return jbxvt_xcb_screen->default_colormap;
+	static xcb_colormap_t c;
+	return c ? c : (c = get_xcb_screen(xc)->default_colormap);
 }
