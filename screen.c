75ab86cc0fb38663c0028181c72e3b03d8f411cc
Jeffrey E. Bedard <jefbed@gmail.com>
2016-11-21 23:39:39 -0500

    Moved module static screen array to jbxvt_get_screens().  Store current screen as an index into the array.

--- screen.c
+++ screen.c
@@ -13,35 +13,45 @@
 #include "scroll.h"
 #include "size.h"
 #include <string.h>
-static struct JBXVTScreen screens[2], * current;
-struct JBXVTScreen * jbxvt_get_screen(void)
+static uint8_t screen_index; // current screen
+static struct JBXVTScreen * jbxvt_get_screens(void)
 {
-	return current ? current : (current = screens);
+	static struct JBXVTScreen screens[2];
+	return screens;
 }
 struct JBXVTScreen * jbxvt_get_screen_at(const uint8_t i)
 {
-	return &screens[i];
+	return jbxvt_get_screens() + i;
+}
+struct JBXVTScreen * jbxvt_get_screen(void)
+{
+	return jbxvt_get_screen_at(screen_index);
 }
 static void efill_area(const struct JBDim c)
 {
+	struct JBXVTScreen * restrict current = jbxvt_get_screen();
 	for (int_fast16_t y = c.h - 1; y >= 0; --y) {
 		memset(current->text[y], 'E', c.w);
 		memset(current->rend[y], 0, c.w << 2);
 	}
+}
+static void home(xcb_connection_t * restrict xc)
+{
+	jbxvt_move(xc, 0, 0, 0);
 }
 // Set all chars to 'E'
 void jbxvt_efill(xcb_connection_t * xc)
 {
 	LOG("jbxvt_efill");
 	// Move to cursor home in order for all characters to appear.
-	jbxvt_move(xc, 0, 0, 0);
+	home(xc);
 	efill_area(jbxvt_get_char_size());
 	jbxvt_repaint(xc);
 }
 //  Change between the alternate and the main screens
 void jbxvt_change_screen(xcb_connection_t * xc, const bool mode_high)
 {
-	current = (screens + mode_high);
+	screen_index = mode_high ? 1 : 0;
 	jbxvt_get_modes()->charsel = 0;
 	/*  Do not call jbxvt_erase_screen(JBXVT_ERASE_ALL) here--It causes
 	    corruption of the saved line data in this context.  Test case:
@@ -51,7 +61,7 @@ void jbxvt_change_screen(xcb_connection_t * xc, const bool mode_high)
 	    <scroll up>
 	 */
 	jbxvt_reset(xc);
-	jbxvt_move(xc, 0, 0, 0);
+	home(xc);
 	jbxvt_erase_screen(xc, JBXVT_ERASE_AFTER);
 }
 // Scroll from top to current bottom margin count lines, moving cursor
@@ -60,6 +70,6 @@ void jbxvt_index_from(xcb_connection_t * xc,
 {
 	jbxvt_set_scroll(xc, 0);
 	jbxvt_draw_cursor(xc);
-	scroll(xc, top, current->margin.b, count);
+	scroll(xc, top, jbxvt_get_screen()->margin.b, count);
 	jbxvt_draw_cursor(xc);
 }
