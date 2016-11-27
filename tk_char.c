20b5bde4f54d877a5f820de5098e4d82ebf3735a
Jeffrey E. Bedard <jefbed@gmail.com>
2016-11-26 21:01:17 -0500

    Split out vertical_tab().  Use jbxvt_get_margin() and jbxvt_get_y().

--- tk_char.c
+++ tk_char.c
@@ -2,6 +2,7 @@
 #include "tk_char.h"
 #include "cmdtok.h"
 #include "command.h"
+#include "cursor.h"
 #include "libjb/log.h"
 #include "mode.h"
 #include "scr_move.h"
@@ -10,26 +11,28 @@
 #include "tab.h"
 #include "tk_char.h"
 #include <stdio.h>
-static void form_feed(xcb_connection_t * xc)
+static void form_feed(xcb_connection_t * restrict xc)
 {
-	const struct JBDim m = jbxvt_get_current_screen()->margin;
+	const struct JBDim m = *jbxvt_get_margin();
 	jbxvt_move(xc, 0, m.top, 0);
 	if (jbxvt_get_modes()->decpff)
 		dprintf(jbxvt_get_fd(), "FF");
 	scroll(xc, m.top, m.bottom, m.bottom - m.top);
 }
+static void vertical_tab(xcb_connection_t * restrict xc)
+{
+	const uint8_t mt = jbxvt_get_margin()->top;
+	for (uint8_t i = jbxvt_get_y(); i % 8; ++i)
+		  jbxvt_index_from(xc, 1, mt);
+}
 void jbxvt_handle_tk_char(xcb_connection_t * xc, const uint8_t tk_char)
 {
 	switch (tk_char) {
 	case '\n': // handle line feed
-		jbxvt_index_from(xc, 1, jbxvt_get_current_screen()->margin.t);
+		jbxvt_index_from(xc, 1, jbxvt_get_margin()->top);
 		break;
-	case 013: { // vertical tab
-		struct JBXVTScreen * restrict s = jbxvt_get_current_screen();
-		const uint8_t mt = s->margin.t;
-		for (uint8_t i = s->cursor.y; i % 8; ++i)
-			  jbxvt_index_from(xc, 1, mt);
-	}
+	case 013: // vertical tab
+		vertical_tab(xc);
 		break;
 	case '\f': // form feed
 		form_feed(xc);
