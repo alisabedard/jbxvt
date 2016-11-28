e1ba74e7d9e315cda57a54eb15812da48f493934
Jeffrey E. Bedard <jefbed@gmail.com>
2016-11-28 00:30:16 -0500

    Broke long lines.  Revised comments.  Removed enumeration name.

--- JBXVTToken.h
+++ JBXVTToken.h
@@ -4,15 +4,14 @@
 #define JBXVT_JBXVTTOKEN_H
 #include <stdint.h>
 #include "JBXVTTokenType.h"
-enum JBXVTTokenLimits {
-	JBXVT_TOKEN_MAX_LENGTH =	63,		// max length of a string token
-	JBXVT_TOKEN_MAX_ARGS =	8	// max # of numeric arguments
-};
+enum { JBXVT_TOKEN_MAX_LENGTH = 63, JBXVT_TOKEN_MAX_ARGS = 8 };
 /*  Structure used to represent a piece of input from the program
  *  or an interesting X event.  */
 struct JBXVTToken{
-	int16_t arg[JBXVT_TOKEN_MAX_ARGS]; // first numerical arguments
-	uint8_t string[JBXVT_TOKEN_MAX_LENGTH + 1]; // the text for stringtokens
+	// numeric arguments:
+	int16_t arg[JBXVT_TOKEN_MAX_ARGS];
+	// text for string tokens:
+	uint8_t string[JBXVT_TOKEN_MAX_LENGTH + 1];
 	enum JBXVTTokenType type;
 	union {
 		uint16_t private;	// non zero for priv ctl sequences
