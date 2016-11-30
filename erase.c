7a96eba24c0e81a29a408d5d5d0e180d767b502f
Jeffrey E. Bedard <jefbed@gmail.com>
2016-11-30 03:53:24 -0500

    Tightly scoped * s and y in del().

--- erase.c
+++ erase.c
@@ -37,23 +37,25 @@ static inline uint16_t get_height(void)
 }
 static void del(xcb_connection_t * xc, uint16_t col, uint16_t width)
 {
-	struct JBXVTScreen * restrict s = jbxvt_get_current_screen();
 	{ // cw scope
 		const uint16_t cw = get_width();
 		if (col + width > cw) // keep in screen
 			width = cw - col;
 	}
-	const int16_t y = s->cursor.y;
-	memset(s->text[y] + col, 0, width);
-	memset(s->rend[y] + col, 0, width << 2);
-	{ // f scope
-		const struct JBDim f = jbxvt_get_font_size();
-		xcb_clear_area(xc, 0, jbxvt_get_vt_window(xc), col * f.w,
-			y * f.h, width * f.w, f.h);
+	{ // * s, y scope
+		struct JBXVTScreen * restrict s = jbxvt_get_current_screen();
+		const int16_t y = s->cursor.y;
+		memset(s->text[y] + col, 0, width);
+		memset(s->rend[y] + col, 0, width << 2);
+		{ // f scope
+			const struct JBDim f = jbxvt_get_font_size();
+			xcb_clear_area(xc, 0, jbxvt_get_vt_window(xc),
+				col * f.w, y * f.h, width * f.w, f.h);
+		}
+		s->wrap[y] = false;
+		s->dwl[y] = false;
 	}
 	xcb_flush(xc);
-	s->wrap[y] = false;
-	s->dwl[y] = false;
 }
 // Erase the specified portion of a line.
 void jbxvt_erase_line(xcb_connection_t * xc, const int8_t mode)
