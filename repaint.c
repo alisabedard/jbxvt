7c6140afe1ddccfd4632192d6ba2fa5dd762d536
Jeffrey E. Bedard <jefbed@gmail.com>
2016-12-04 01:45:35 -0500

    Split out get_render_length().  Used rstyle_t.

--- repaint.c
+++ repaint.c
@@ -11,28 +11,31 @@
 #include "show_selection.h"
 #include "size.h"
 #include "window.h"
+static uint16_t get_render_length(rstyle_t * rvec, const uint16_t len)
+{
+	uint_fast16_t i = 0;
+	for(;i < len && rvec[i] == rvec[0]; ++i)
+		;
+	return i;
+}
 /* Display the string using the rendition vector
    at the screen coordinates.  */
 static void paint_rvec_text(xcb_connection_t * xc,
-	uint8_t * str, uint32_t * rvec,
-	int16_t len, struct JBDim p, const bool dwl)
+	uint8_t * str, rstyle_t * rvec,
+	uint16_t len, struct JBDim p, const bool dwl)
 {
-	if (!rvec || !str)
-		  return;
-	while (len > 0) {
-		uint32_t r;
-		int_fast16_t i;
-		/* Find the length for which
-		   the current rend val applies.  */
-		for (i = 0, r = *rvec; i < len && rvec[i] == r; ++i)
-			;
-		// draw
-		jbxvt_paint(xc, str, r, i, p, dwl);
-		// advance to next block
-		p.x += i * jbxvt_get_font_size().width;
-		str += i;
-		rvec += i;
-		len -= i;
+	/* Save the retrieved font width to avoid function call overhead.  */
+	static uint8_t fw;
+	if (!fw)
+		fw = jbxvt_get_font_size().width;
+	while (len) {
+		const uint16_t rlen = get_render_length(rvec, len);
+		jbxvt_paint(xc, str, rvec[0], rlen, p, dwl);
+		// advance to next block:
+		p.x += rlen * fw;
+		str += rlen;
+		rvec += rlen;
+		len -= rlen;
 	}
 }
 __attribute__((nonnull))
