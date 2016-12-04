db649a1f8c68e48d22c26bd2ceb84fa2fc60033a
Jeffrey E. Bedard <jefbed@gmail.com>
2016-12-04 03:16:04 -0500

    Used uint_fast16_t for get_render_length().  Changed iteration to while loop. Revised paint_rvec_text() comment.  Changed implementation of paint_rvec_text to a complex for loop.  Tightly scoped x and w in show_scroll_history().

--- repaint.c
+++ repaint.c
@@ -10,32 +10,22 @@
 #include "show_selection.h"
 #include "size.h"
 #include "window.h"
-static uint16_t get_render_length(rstyle_t * rvec, const uint16_t len)
+static uint_fast16_t get_render_length(const rstyle_t * rvec, const uint16_t len)
 {
 	uint_fast16_t i = 0;
-	for(;i < len && rvec[i] == rvec[0]; ++i)
-		;
+	while (i < len && rvec[i] == rvec[0])
+		++i;
 	return i;
 }
-/* Display the string using the rendition vector
-   at the screen coordinates.  */
+// Display the string using the render vector at the screen coordinates.
 static void paint_rvec_text(xcb_connection_t * xc,
-	uint8_t * str, rstyle_t * rvec,
+	uint8_t * str, const rstyle_t * rvec,
 	uint16_t len, struct JBDim p, const bool dwl)
 {
-	/* Save the retrieved font width to avoid function call overhead.  */
-	static uint8_t fw;
-	if (!fw)
-		fw = jbxvt_get_font_size().width;
-	while (len) {
-		const uint16_t rlen = get_render_length(rvec, len);
-		jbxvt_paint(xc, str, rvec[0], rlen, p, dwl);
-		// advance to next block:
-		p.x += rlen * fw;
-		str += rlen;
-		rvec += rlen;
-		len -= rlen;
-	}
+	const uint8_t fw = jbxvt_get_font_size().width;
+	for (uint_fast16_t i; len; len -= i, str += i, rvec += i, p.x += i * fw)
+		jbxvt_paint(xc, str, *rvec, i = get_render_length(rvec, len), p,
+			dwl);
 }
 __attribute__((nonnull))
 static int_fast16_t show_scroll_history(xcb_connection_t * xc,
@@ -46,12 +36,14 @@ static int_fast16_t show_scroll_history(xcb_connection_t * xc,
 	if (line > chars.h || i < 0)
 		return line;
 	struct JBXVTSavedLine * sl = &jbxvt_get_saved_lines()[i];
-	paint_rvec_text(xc, sl->text, sl->rend, chars.width, *p, sl->dwl);
+	paint_rvec_text(xc, sl->text, sl->rend, sl->size, *p, sl->dwl);
 	const struct JBDim f = jbxvt_get_font_size();
-	const int16_t x = sl->size * f.width;
-	const uint16_t w = chars.width * f.width - x;
-	xcb_clear_area(xc, false, jbxvt_get_vt_window(xc), x, p->y, w,
-		f.height);
+	{ // x, w scope
+		const int16_t x = sl->size * f.width;
+		const uint16_t w = chars.width * f.width - x;
+		xcb_clear_area(xc, 0, jbxvt_get_vt_window(xc), x, p->y,
+			w, f.height);
+	}
 	p->y += f.height;
 	return show_scroll_history(xc, p, line + 1, i - 1);
 }
