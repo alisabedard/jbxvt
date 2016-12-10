f4e63fe1602b04fb3230f73812ef9c6244a93157
Jeffrey E. Bedard <jefbed@gmail.com>
2016-12-10 13:18:29 -0500

    Renamed handle_sgr to sgr.

--- handle_sgr.h
+++
@@ -1,8 +0,0 @@
-#ifndef JBXVT_HANDLE_SGR_H
-#define JBXVT_HANDLE_SGR_H
-#include <xcb/xcb.h>
-#include "JBXVTToken.h"
-void jbxvt_handle_sgr(xcb_connection_t * xc,
-	struct JBXVTToken * restrict token)
-	__attribute__((hot));
-#endif//!JBXVT_HANDLE_SGR_H
