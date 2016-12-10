37e3d5087ffe9730760e6bded56b927140371532
Jeffrey E. Bedard <jefbed@gmail.com>
2016-12-10 02:26:55 -0500

    Removed unnecessary call to jbxvt_draw_cursor().  Simplified disabling of debug logging.

--- edit.c
+++ edit.c
@@ -1,6 +1,7 @@
 /*  Copyright 2016, Jeffrey E. Bedard
     Copyright 1992, 1997 John Bovey,
     University of Kent at Canterbury.*/
+//#undef DEBUG
 #include "edit.h"
 #include "cursor.h"
 #include "font.h"
@@ -10,11 +11,6 @@
 #include "screen.h"
 #include "size.h"
 #include "window.h"
-#define DEBUG_EDIT
-#ifndef DEBUG_EDIT
-#undef LOG
-#define LOG(...)
-#endif//!DEBUG_EDIT
 static inline uint16_t get_copy_width(const uint8_t count)
 {
 	const uint16_t w = jbxvt_get_char_size().w - count - jbxvt_get_x();
@@ -40,9 +36,7 @@ void jbxvt_edit_characters(xcb_connection_t * xc, const uint8_t count,
 	   what determines insertion or deletion.  */
 	if (delete)
 		JB_SWAP(int16_t, x[0], x[1]);
-	jbxvt_draw_cursor(xc);
 	copy_area(xc, x, c.y, count);
 	jbxvt_get_current_screen()->wrap_next = 0;
 	jbxvt_draw_cursor(xc);
-
 }
