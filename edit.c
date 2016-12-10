f8abc96ce1e098057362f3fed44183cb032d648f
Jeffrey E. Bedard <jefbed@gmail.com>
2016-12-10 02:47:40 -0500

    Save and pass along retrieved font size information.

--- edit.c
+++ edit.c
@@ -11,32 +11,34 @@
 #include "screen.h"
 #include "size.h"
 #include "window.h"
-static inline uint16_t get_copy_width(const uint8_t count)
+static uint16_t get_copy_width(const uint8_t count, const uint8_t
+	font_width)
 {
 	const uint16_t w = jbxvt_get_char_size().w - count - jbxvt_get_x();
-	return w * jbxvt_get_font_size().width;
+	return w * font_width;
 }
 static void copy_area(xcb_connection_t * restrict xc,
 	const int16_t * restrict x, const int16_t y,
-	const uint8_t count)
+	const uint8_t count, const struct JBDim font_size)
 {
 	const xcb_window_t v = jbxvt_get_vt_window(xc);
-	xcb_copy_area(xc, v, v, jbxvt_get_text_gc(xc), x[0], y,
-		x[1], y, get_copy_width(count),
-		jbxvt_get_font_size().height);
+	xcb_copy_area(xc, v, v, jbxvt_get_text_gc(xc), x[0], y, x[1],
+		y, get_copy_width(count, font_size.width),
+		font_size.height);
 }
 void jbxvt_edit_characters(xcb_connection_t * xc, const uint8_t count,
 	const bool delete)
 {
 	LOG("jbxvt_edit_characters(count: %d, delete: %s)", count,
 		delete ? "true" : "false");
-	const struct JBDim c = jbxvt_chars_to_pixels(jbxvt_get_cursor());
+	const struct JBDim c = jbxvt_chars_to_pixels(jbxvt_get_cursor()),
+	      f = jbxvt_get_font_size();
 	int16_t x[2] = {c.x, x[0] + count * jbxvt_get_font_size().width};
 	/* Whether or not x[0] and x[1] are swapped here is
 	   what determines insertion or deletion.  */
 	if (delete)
 		JB_SWAP(int16_t, x[0], x[1]);
-	copy_area(xc, x, c.y, count);
+	copy_area(xc, x, c.y, count, f);
 	jbxvt_get_current_screen()->wrap_next = 0;
 	jbxvt_draw_cursor(xc);
 }
