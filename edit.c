c0eb48ec723444821525a6fd2687a3f81926dc5e
Jeffrey E. Bedard <jefbed@gmail.com>
2016-12-11 04:42:41 -0500

    Added memmove call to make sure the offscreen buffer also receives edits.

--- edit.c
+++ edit.c
@@ -3,11 +3,14 @@
     University of Kent at Canterbury.*/
 //#undef DEBUG
 #include "edit.h"
+#include <string.h>
 #include "cursor.h"
 #include "font.h"
 #include "libjb/log.h"
 #include "libjb/macros.h"
 #include "paint.h"
+#include "repaint.h"
+#include "scr_reset.h"
 #include "screen.h"
 #include "size.h"
 #include "window.h"
@@ -18,27 +21,28 @@ static uint16_t get_copy_width(const uint8_t count, const uint8_t
 	return w * font_width;
 }
 static void copy_area(xcb_connection_t * restrict xc,
-	const int16_t * restrict x, const int16_t y,
-	const uint8_t count, const struct JBDim font_size)
+	const int16_t * restrict x, const uint8_t count)
 {
 	const xcb_window_t v = jbxvt_get_vt_window(xc);
-	xcb_copy_area(xc, v, v, jbxvt_get_text_gc(xc), x[0], y, x[1],
-		y, get_copy_width(count, font_size.width),
-		font_size.height);
+	const struct JBDim f = jbxvt_get_font_size();
+	const int16_t y = jbxvt_get_y() * f.height;
+	xcb_copy_area(xc, v, v, jbxvt_get_text_gc(xc), f.w * x[0], y,
+		f.w * x[1], y, get_copy_width(count, f.w), f.h);
 }
-void jbxvt_edit_characters(xcb_connection_t * xc, const uint8_t count,
-	const bool delete)
+void jbxvt_edit_characters(xcb_connection_t * xc,
+	const uint8_t count, const bool delete)
 {
 	LOG("jbxvt_edit_characters(count: %d, delete: %s)", count,
 		delete ? "true" : "false");
-	const struct JBDim c = jbxvt_chars_to_pixels(jbxvt_get_cursor()),
-	      f = jbxvt_get_font_size();
-	int16_t x[2] = {c.x, x[0] + count * f.width};
-	/* Whether or not x[0] and x[1] are swapped here is
-	   what determines insertion or deletion.  */
+	struct JBXVTLine * l = jbxvt_get_line(jbxvt_get_y());
+	const int16_t x = jbxvt_get_x();
+	uint8_t * t = l->text;
+	uint8_t * a = t + x;
+	uint8_t * b = a + count;
 	if (delete)
-		JB_SWAP(int16_t, x[0], x[1]);
-	copy_area(xc, x, c.y, count, f);
-	jbxvt_get_current_screen()->wrap_next = 0;
+		JB_SWAP(uint8_t *, a, b);
+	memmove(a, b, count);
+	const int16_t p[] = {a - t, b - t};
+	copy_area(xc, p, count);
 	jbxvt_draw_cursor(xc);
 }
