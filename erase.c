97d658917651618bbe19953163b68f3f6af3d600
Jeffrey E. Bedard <jefbed@gmail.com>
2016-12-13 12:53:40 -0500

    Added debug logging of modes.

--- erase.c
+++ erase.c
@@ -2,6 +2,7 @@
     Copyright 1992, 1997 John Bovey,
     University of Kent at Canterbury.*/
 //#undef DEBUG
+#define LOG_LEVEL 6
 #include "erase.h"
 #include <string.h>
 #include "cursor.h"
@@ -28,13 +29,16 @@ static void clear_area(xcb_connection_t * restrict xc,
 }
 static uint16_t get_limited_width(const uint16_t col, const uint16_t width)
 {
-	const uint16_t cw = get_width();
+	const int cw = get_width();
 	// keep col + width within the screen width
 	return (col + width > cw) ? cw - col : width;
 }
 static void delete(xcb_connection_t * restrict xc, const uint16_t col,
 	uint16_t width)
 {
+#if LOG_LEVEL > 5
+	LOG("\tdelete(xc, col:%d, width: %d)", col, width);
+#endif//LOG_LEVEL>5
 	width = get_limited_width(col, width);
 	struct JBXVTScreen * restrict s = jbxvt_get_current_screen();
 	const int16_t y = s->cursor.y;
@@ -45,14 +49,31 @@ static void delete(xcb_connection_t * restrict xc, const uint16_t col,
 	l->wrap = l->dwl = false;
 }
 static inline void delete_after(xcb_connection_t * restrict xc,
-	const int16_t x)
+	const uint16_t col)
 {
-	delete(xc, x, get_width() - x);
+	delete(xc, col, get_width() - col);
 }
+#ifdef DEBUG
+static char * debug_mode(const int8_t mode)
+{
+	switch(mode) {
+	case 0:
+		return "0:After";
+	case 1:
+		return "1:Before";
+	case 2:
+		return "2:All";
+	case 3:
+		return "3:Saved";
+	default:
+		return "?:Unknown";
+	}
+}
+#endif//DEBUG
 // Erase the specified portion of a line.
 void jbxvt_erase_line(xcb_connection_t * xc, const int8_t mode)
 {
-	LOG("jbxvt_erase_line(xc, mode: %d)", mode);
+	LOG("jbxvt_erase_line(xc, mode: %s)", debug_mode(mode));
 	jbxvt_set_scroll(xc, 0);
 	switch (mode) {
 	case JBXVT_ERASE_ALL:
@@ -92,7 +113,7 @@ static bool assign_range(xcb_connection_t * restrict xc,
 // Erase the specified portion of the screen.
 void jbxvt_erase_screen(xcb_connection_t * xc, const int8_t mode)
 {
-	LOG("jbxvt_erase_screen(mode=%d)", mode);
+	LOG("jbxvt_erase_screen(mode=%s)", debug_mode(mode));
 	struct JBDim range;
 	if (!assign_range(xc, mode, &range))
 		return;
