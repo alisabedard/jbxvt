659355a5441e08b0257e93c9cbab871761de8cf8
Jeffrey E. Bedard <jefbed@gmail.com>
2016-12-13 23:33:22 -0500

    Added conditional to disable logging at level 0.  Set level to 0.

--- string.c
+++ string.c
@@ -1,8 +1,10 @@
 /*  Copyright 2016, Jeffrey E. Bedard
     Copyright 1992, 1997 John Bovey,
     University of Kent at Canterbury.*/
-//#undef DEBUG
-#define LOG_LEVEL 3
+#define LOG_LEVEL 0
+#if LOG_LEVEL == 0
+#undef DEBUG
+#endif
 #include "string.h"
 #include <string.h>
 #include <unistd.h>
