43cfbe0843bf0978c6ac63938ae7ddb1e69b9ad2
Jeffrey E. Bedard <jefbed@gmail.com>
2016-12-24 04:02:51 -0500

    Used a more recent revision.

--- jbxvt.c
+++ jbxvt.c
@@ -1,21 +1,27 @@
 // Copyright 2016, Jeffrey E. Bedard
-#include "JBXVTOptions.h"
 #include "command.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <xcb/xcb.h>
+#include "JBXVTOptions.h"
+#include "color.h"
 #include "config.h"
 #include "cursor.h"
 #include "display.h"
+#include "font.h"
+#include "libjb/JBDim.h"
+#include "libjb/util.h"
 #include "tab.h"
 #include "window.h"
 #include "xevents.h"
 #include "xvt.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 static char ** parse_command_line(const int argc, char ** argv,
 	struct JBXVTOptions * o)
 {
 	static const char * optstr = "B:b:C:c:D:d:eF:f:hI:R:S:svx:y:";
-	int8_t opt;
+	char opt;
 	while((opt=getopt(argc, argv, optstr)) != -1) {
 		switch (opt) {
 		case 'B': // bold font
@@ -71,26 +77,20 @@ usage:
 	return NULL;
 #endif//OPENBSD
 }
-static void set_default_options(struct JBXVTOptions * restrict o)
-{
-	o->font.normal = JBXVT_NORMAL_FONT;
-	o->font.bold = JBXVT_BOLD_FONT;
-	o->font.italic = JBXVT_ITALIC_FONT;
-	o->color.bg = JBXVT_BACKGROUND_COLOR;
-	o->color.fg = JBXVT_FOREGROUND_COLOR;
-	o->size.cols = JBXVT_COLUMNS;
-	o->size.rows = JBXVT_ROWS;
-	o->position = (struct JBDim){};
-	o->screen = 0;
-	o->show_scrollbar = false;
-}
-
 static xcb_connection_t * handle_options(const int argc, char ** argv,
 	char *** com_argv)
 {
-	struct JBXVTOptions o;
-	set_default_options(&o);
-	// Override defaults
+	// Set defaults:
+	struct JBXVTOptions o = {
+		.font.normal = JBXVT_NORMAL_FONT,
+		.font.bold = JBXVT_BOLD_FONT,
+		.font.italic = JBXVT_ITALIC_FONT,
+		.color.bg = JBXVT_BACKGROUND_COLOR,
+		.color.fg = JBXVT_FOREGROUND_COLOR,
+		.size.cols = JBXVT_COLUMNS,
+		.size.rows = JBXVT_ROWS,
+	};
+	// Override defaults:
 	*com_argv = parse_command_line(argc, argv, &o);
 	/* jbxvt_init_display must come
 	   after parse_command_line */
@@ -99,8 +99,7 @@ static xcb_connection_t * handle_options(const int argc, char ** argv,
 static xcb_connection_t * handle_command(const int argc, char ** argv)
 {
 	char ** com_argv;
-	xcb_connection_t * xc = handle_options(argc,
-		argv, &com_argv);
+	xcb_connection_t * xc = handle_options(argc, argv, &com_argv);
 	jbxvt_set_tab(-2, false); // Set up the tab stops
 	jbxvt_map_window(xc);
 	jb_check(setenv("TERM", JBXVT_ENV_TERM, true) != -1,
@@ -110,14 +109,16 @@ static xcb_connection_t * handle_command(const int argc, char ** argv)
 	jbxvt_init_command_module(com_argv);
 	return xc;
 }
-/*  Run the command in a subprocess and return a file descriptor for the
- *  master end of the pseudo-teletype pair with the command talking to
- *  the slave.  */
+/*  Run the command in a subprocess and return a file descriptor for
+ *  the master end of the pseudo-teletype pair with the command
+ *  talking to the slave.  */
 int main(int argc, char ** argv)
 {
 	xcb_connection_t * xc = handle_command(argc, argv);
-	jbxvt_get_wm_del_win(xc); // initialize property
-	for (;;) // app loop
-		jbxvt_parse_token(xc);
+	(void)jbxvt_get_wm_del_win(xc); // initialize property
+	while (jbxvt_parse_token(xc))
+		;
+	xcb_disconnect(xc);
+	free(xc);
 	return 0;
 }
