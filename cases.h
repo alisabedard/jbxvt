d1fd077f279b2e0fd59ba97b0798efbe740551f1
Jeffrey E. Bedard <jefbed@gmail.com>
2017-04-15 23:45:30 -0400

    Created header for casegen framework macros.

---
+++ cases.h
@@ -0,0 +1,14 @@
+// Copyright 2017, Jeffrey E. Bedard
+#ifndef JBXVT_CASES_H
+#define JBXVT_CASES_H
+// Note:  Cast to (void) to avoid warnings about unused arguments
+#define PARMS xcb_connection_t * xc, struct JBXVTToken * token
+#define NOPARM_XC() (void)xc;
+#define NOPARM_TOKEN() (void)token;
+#define NOPARM() NOPARM_XC(); NOPARM_TOKEN();
+#define HANDLE(name) static void jbxvt_handle_JBXVT_TOKEN_##name(PARMS)
+#define EXTERN_ALIAS(alias, source) static void \
+	(* jbxvt_handle_JBXVT_TOKEN_##alias)(PARMS) = source
+#define ALIAS(alias, source) EXTERN_ALIAS(alias, \
+	jbxvt_handle_JBXVT_TOKEN_##source)
+#endif//!JBXVT_CASES_H
