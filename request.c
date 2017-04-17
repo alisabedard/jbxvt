7adc833cab36dbd3c1b281b45ce3676c8aab27c3
Jeffrey E. Bedard <jefbed@gmail.com>
2017-04-16 23:37:29 -0400

    Moved jbxvt_handle_JBXVT_TOKEN_LL() to request.c.

--- request.c
+++ request.c
@@ -5,7 +5,61 @@
 #include "JBXVTToken.h"
 #include "cmdtok.h"
 #include "command.h"
+#include "cursor.h"
 #include "libjb/log.h"
+// Load LEDs, SCUSR and SCA
+void jbxvt_handle_JBXVT_TOKEN_LL(void * xc __attribute__((unused)),
+	struct JBXVTToken * token)
+{
+	int16_t * restrict t = token->arg;
+	LOG("t[0]: %d, t[1]: %d", t[0], t[1]);
+	switch (t[1]) {
+	case 0:
+	case ' ': // SCUSR
+		LOG("SCUSR");
+		jbxvt_set_cursor_attr(t[0]);
+		break;
+	case '"': // SCA - Select Character protection Attribute
+		switch(t[0]) {
+		default:
+		case 0:
+		case 2: // DECSED and DECSEL can erase
+			LOG("SCA can erase");
+			break;
+		case 1: // DECSED and DECSEL cannot erase
+			LOG("SCA cannot erase");
+			break;
+		}
+		break;
+	default: // LL
+		switch (t[0]) {
+		default:
+		case 0:
+			LOG("LL clear all LEDs");
+			break;
+		case 1:
+			LOG("LL light num lock");
+			break;
+		case 2:
+			LOG("LL light caps lock");
+			break;
+		case 3:
+			LOG("LL light scroll lock");
+			break;
+		case 21:
+			LOG("LL extinguish num lock");
+			break;
+		case 22:
+			LOG("LL extinguish caps lock");
+			break;
+		case 23:
+			LOG("LL extinguish scroll lock");
+			break;
+		}
+		LOG("LL -- unimplemented");
+	}
+
+}
 void jbxvt_handle_JBXVT_TOKEN_REQTPARAM(void * xc __attribute__((unused)),
 	struct JBXVTToken * token)
 {
--- request.h
+++ request.h
@@ -3,6 +3,7 @@
 #ifndef JBXVT_REQUEST_H
 #define JBXVT_REQUEST_H
 struct JBXVTToken;
-void jbxvt_handle_JBXVT_TOKEN_REQTPARAM(void * xc __attribute__((unused)),
-	struct JBXVTToken * token);
+// Load LEDs, SCUSR and SCA
+void jbxvt_handle_JBXVT_TOKEN_LL(void * xc, struct JBXVTToken * token);
+void jbxvt_handle_JBXVT_TOKEN_REQTPARAM(void * xc, struct JBXVTToken * token);
 #endif//!JBXVT_REQUEST_H
