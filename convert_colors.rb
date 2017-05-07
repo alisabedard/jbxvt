33ffe0d8aad67dc70686c48bf2647a733bf9397a
Jeffrey E. Bedard <jefbed@gmail.com>
2017-05-07 02:02:12 -0400

    Used file block for color_index.txt I/O.  Used f.each_line to iterate through file f in order to gain block notation in proceeding section.  Used enumerated variable s instead of global $_ as source of string.

--- convert_colors.rb
+++ convert_colors.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 # Copyright 2017, Jeffrey E. Bedard
-f=File.open "color_index.txt", "r"
 o_name="color_index.h"
 o=File.open o_name, "w"
 o.write "// Copyright 2017, Jeffrey E. Bedard\n" +
@@ -8,11 +7,14 @@ o.write "// Copyright 2017, Jeffrey E. Bedard\n" +
 	"#define JBXVT_COLOR_INDEX_H\n" +
 	"#include <stdint.h>\n" +
 	"static uint32_t jbxvt_color_index[256] = {\n"
-while f.gets
-	s = $_.split ':'
-	if s.length > 1 # ignore blank lines
-		s[1]=s[1].strip # delete trailing '\n'
-		o.write "\t[#{s[0]}] = 0x#{s[1]},\n"
+#f=File.open "color_index.txt", "r"
+File.open "color_index.txt", "r" do |f|
+	f.each_line do |s|
+		s = s.split ':'
+		if s.length > 1 # ignore blank lines
+			s[1] = s[1].strip # delete trailing '\n'
+			o.write "\t[#{s[0]}] = 0x#{s[1]},\n"
+		end
 	end
 end
 o.write "};\n#endif//!JBXVT_COLOR_INDEX_H\n"
