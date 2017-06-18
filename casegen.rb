ea7b7645fb4f76f4a7185e574cee8aba2bfd22fc
Jeffrey E. Bedard <jefbed@gmail.com>
2017-06-18 01:47:25 -0400

    Rewrote in an object oriented manner, using the Coder and Writer classes inside of the CaseGenerator module.  Execute if argv 0 is the source file.

--- casegen.rb
+++ casegen.rb
@@ -1,27 +1,69 @@
 #!/usr/bin/env ruby
 # Copyright 2017 Jeffrey E. Bedard
-out="cases.c"
-file = File.new out, "w"
-tokens=%w(ALN CHA CHT CPL CNL CS_ALT_G1 CS_ALT_G2 CS_ALT_G3 CS_G0 CS_G1 CS_G2
-CS_G3 CUB CUD CUF CUP CUU DA DCH DL DSR DWL ECH ED EL ELR ENTGM52 EXTGM52 HOME
-HPA HPR HTS HVP ICH ID IL IND LL MC NEL PAM PM PNM RC REQTPARAM RESET RI RIS
-RQM S7C1T S8C1T SAVEPM SBGOTO SBSWITCH SC SD SET SELINSRT SGR SS2 SS3 ST STBM
-SU SWL TBC TXTPAR VPA VPR)
-nolog_tokens=%w(CHAR STRING)
-stubs=%w(APC DHLT DHLB EPA OSC SOS SPA)
-tokens.each do |token|
-	token = "JBXVT_TOKEN_#{token}"
-	file.write "case #{token}:\n\tLOG(\"#{token}\");\n" +
-		"\tjbxvt_handle_#{token}(xc, &token);\n\tbreak;\n"
+module CaseGenerator
+	class Writer
+		def initialize(output_file_name = "cases.c")
+			@file = File.new output_file_name, "w"
+		end
+		def write(string)
+			@file.write string
+		end
+		def newline
+			write "\n"
+		end
+	end
+	class Coder
+		def initialize(_token)
+			@token = _token
+			set_prefix
+		end
+		def set_prefix
+			@token = "JBXVT_TOKEN_#{@token}"
+		end
+		def get_break
+			return "\tbreak;\n"
+		end
+		def get_logged
+			return "case #{@token}:\n" +
+				"\tLOG(\"#{@token}\");\n" +
+				"\tjbxvt_handle_#{@token}(xc, &token);\n" +
+				get_break
+		end
+		def get_not_logged
+			return "case #{@token}:\n" +
+				"\tjbxvt_handle_#{@token}(xc, &token);\n" +
+				get_break
+		end
+		def get_stub
+			return "case #{@token}:\n\tLOG(\"FIXME: " +
+				"#{@token} not implemented\");\n" +
+				get_break
+		end
+	end
+	def CaseGenerator.main
+		w = Writer.new
+		_tokens=%w(ALN CHA CHT CPL CNL CS_ALT_G1 CS_ALT_G2 CS_ALT_G3
+		CS_G0 CS_G1 CS_G2 CS_G3 CUB CUD CUF CUP CUU DA DCH DL DSR DWL
+		ECH ED EL ELR ENTGM52 EXTGM52 HOME HPA HPR HTS HVP ICH ID IL
+		IND LL MC NEL PAM PM PNM RC REQTPARAM RESET RI RIS RQM S7C1T
+		S8C1T SAVEPM SBGOTO SBSWITCH SC SD SET SELINSRT SGR SS2 SS3 ST
+		STBM SU SWL TBC TXTPAR VPA VPR)
+		_nolog_tokens=%w(CHAR STRING)
+		_stubs=%w(APC DHLT DHLB EPA OSC SOS SPA)
+		_tokens.each do |token|
+			c = Coder.new token
+			w.write c.get_logged
+		end
+		_nolog_tokens.each do |token|
+			c = Coder.new token
+			w.write c.get_not_logged
+		end
+		_stubs.each do |token|
+			c = Coder.new token
+			w.write c.get_stub
+		end
+	end
+	if __FILE__ == $0
+		main
+	end
 end
-nolog_tokens.each do |token|
-	token = "JBXVT_TOKEN_#{token}"
-	file.write "case #{token}:\n" +
-		"\tjbxvt_handle_#{token}(xc, &token);\n\tbreak;\n"
-end
-stubs.each do |token|
-	token = "JBXVT_TOKEN_#{token}"
-	file.write "case #{token}:\n" +
-		"\tLOG(\"FIXME: #{token} not implemented\");\n\tbreak;\n" 
-end
-
