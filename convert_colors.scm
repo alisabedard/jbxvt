e4ead97c2d257954bfbecac36443dfb8a780c21c
Jeffrey E. Bedard <jefbed@gmail.com>
2017-06-21 21:32:09 -0400

    Rewrote conditional logic within parse to be easier to understand.  Eliminated duplicate branch by addition of begin.

--- convert_colors.scm
+++ convert_colors.scm
@@ -1,29 +1,33 @@
 ; Copyright 2017, Jeffrey E. Bedard
 (load "libjb/libconvert.scm")
-(letrec ((get_line (lambda (index value) (string-append
-	"\t[" index "] = 0x" value ",\n")))
+(letrec 
+	((format-line (lambda (index value) (string-append
+		"\t[" index "] = 0x" value ",\n")))
 	(parse (lambda (in out) (let*
-	((line (read-line in)))
-	(if (not (eof-object? line))
-		(if (> (string-length line) 1)
-			(let* ((i (string-find-next-char line #\:))
-			(index (string-car line))
-			(value (string-cdr line)))
-			(display (get_line index value) out)
-			(flush-output out)
-			(parse in out))
-			(parse in out))))))
-(convert_colors (lambda (in_file_name out_file_name) (let*
-	((tag "JBXVT_COLOR_INDEX") (varname "jbxvt_color_index")
-	(vartype "uint32_t") (include0 "<stdint.h>")
-	(i (open-input-file in_file_name))
-	(o (open-output-file out_file_name)))
-	(begin-include tag o)
-	(display (add-c-include include0) o)
-	(begin-array-definition vartype varname o)
-	(parse i o)
-	(end-c-definition o)
-	(end-include tag o)
-	(close-port i)
-	(close-port o)))))
-(convert_colors "color_index.txt" "color_index.h"))
+		((line (read-line in)))
+		(if (eof-object? line)
+			#f
+			(begin
+				(if (> (string-length line) 1)
+					(let* ((i (string-find-next-char
+						line #\:))
+						(index (string-car line))
+						(value (string-cdr line)))
+					(display (format-line
+						index value) out)
+					(flush-output out)))
+				(parse in out))))))
+	(convert_colors (lambda (in_file_name out_file_name) (let*
+		((tag "JBXVT_COLOR_INDEX") (varname "jbxvt_color_index")
+		(vartype "uint32_t") (include0 "<stdint.h>")
+		(i (open-input-file in_file_name))
+		(o (open-output-file out_file_name)))
+		(begin-include tag o)
+		(display (add-c-include include0) o)
+		(begin-array-definition vartype varname o)
+		(parse i o)
+		(end-c-definition o)
+		(end-include tag o)
+		(close-port i)
+		(close-port o)))))
+	(convert_colors "color_index.txt" "color_index.h"))
