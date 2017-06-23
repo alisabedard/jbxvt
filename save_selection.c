f4a2b67aa90c72a500782c2c8f572f6223a77d56
Jeffrey E. Bedard <jefbed@gmail.com>
2017-06-23 02:51:28 -0400

    Split out copy_line().  Moved common parameter and variable data to struct CopyData.

--- save_selection.c
+++ save_selection.c
@@ -31,6 +31,31 @@ static bool is_not_on_screen(const struct JBDim * restrict e)
 	return !end_point_on_screen(e[0], c)
 		|| !end_point_on_screen(e[1], c);
 }
+struct CopyData {
+	struct JBDim * endpoints;
+	uint8_t * string;
+	int total;
+	short char_width;
+};
+static int copy_line(const int i, const int j, struct CopyData * restrict d)
+{
+	if (i <= j) {
+		/* Use full screen width if not first or last lines of
+		 * selection, otherwise use the col field in the respective
+		 * end point.  */
+		struct JBDim * restrict e = d->endpoints;
+		const int16_t start = i == e[0].index ? e[0].col : 0;
+		const int16_t end = i == j ? e[1].col : d->char_width - 1;
+		const uint16_t len = end - start;
+		const int new_total = d->total + len;
+		d->string = realloc(d->string, new_total);
+		copy(d->string, jbxvt_get_line(i)->text, start,
+			len, d->total);
+		d->total = new_total;
+		return copy_line(i + 1, j, d);
+	}
+	return d->total;
+}
 /*  Convert the currently marked screen selection as a text string
     and save it as the current saved selection. */
 void jbxvt_save_selection(struct JBXVTSelectionData * sel)
@@ -41,35 +66,26 @@ void jbxvt_save_selection(struct JBXVTSelectionData * sel)
 		sel->end[fwd ? 1 : 0]};
 	/* Use a normal int type for total to avoid overflow and failure to
 	 * detect decrement below 0. */
-	int total = 1;
-	uint8_t * str = malloc(total);
 	// Make sure the selection falls within the screen area:
 	if (is_not_on_screen(e))
 		return; // Invalid end point found
+	struct CopyData d = {.endpoints = e, .total = 1,
+		.char_width = jbxvt_get_char_size().width};
+	d.string = malloc(d.total);
 	// Copy each line in the selection:
-	for (int_fast16_t i = e[0].index, j = e[1].index; i <= j; ++i) {
-		/* Use full screen width if not first or last lines of
-		   selection, otherwise use the col field in the respective
-		   end point.  */
-		const int16_t start = i == e[0].index ? e[0].col : 0;
-		const int16_t end = i == j ? e[1].col
-			: jbxvt_get_char_size().width - 1;
-		const uint16_t len = end - start;
-		str = realloc(str, total + len);
-		copy(str, jbxvt_get_line(i)->text, start, len, total);
-		total += len;
-	}
+	d.total = copy_line(e[0].index, e[1].index, &d);
 	bool last_was_cr = false;
 	// Substitute the first null terminator with a carriage return:
-	for (int_fast16_t i = 0; i < total; ++i)
-		if (str[i] == '\0') {
+	for (int_fast16_t i = 0; i < d.total; ++i)
+		if (d.string[i] == '\0') {
 			if (!last_was_cr) {
-				str[i] = '\r';
+				d.string[i] = '\r';
 				last_was_cr = true;
 			}
 		} else
 			last_was_cr = false;
-	sel->text = str = realloc(str, total);
-	if (str && total > 0) // avoid segmentation fault if total is invalid
-		str[sel->length = --total] = 0; // null termination
+	sel->text = d.string = realloc(d.string, d.total);
+	if (d.string && d.total > 0) /* avoid segmentation fault,
+					if total is invalid. */
+		d.string[sel->length = --d.total] = 0; // add null termination
 }
