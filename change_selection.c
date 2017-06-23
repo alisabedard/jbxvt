400e3aa74ada57a5a1749da194c81054a61e449d
Jeffrey E. Bedard <jefbed@gmail.com>
2017-06-22 21:59:01 -0400

    Rewrote to simplify, eliminating duplication of row data, consolidating arguments in structure InversionData, and calling recursively.

--- change_selection.c
+++ change_selection.c
@@ -9,35 +9,35 @@
 #include "selend.h"
 #include "size.h"
 #include "window.h"
-static void invert(xcb_connection_t * xc, const int16_t rs,
-	const int16_t re, const int16_t cs, const int16_t ce,
-	const uint8_t row1, const uint8_t row2)
+struct InversionData {
+	xcb_connection_t * xc;
+	struct JBDim font_size;
+	struct JBDim start_point, end_point;
+	xcb_window_t window;
+	xcb_gcontext_t gc;
+	uint16_t width_in_chars;
+};
+static void invert_r(const uint8_t current,
+	struct InversionData * i)
 {
-	const struct JBDim csz = jbxvt_get_char_size(),
-	      fsz = jbxvt_get_font_size();
-	for (uint8_t row = row1; row <= row2; row++) {
-		const int16_t x[] = {row == rs ? cs * fsz.w : 0,
-			(row == re ? ce : csz.w) * fsz.w};
-		xcb_poly_fill_rectangle(xc, jbxvt_get_vt_window(xc),
-			jbxvt_get_cursor_gc(xc), 1, &(xcb_rectangle_t){
-			.x = x[0], .y = row * fsz.height,
-			.width = x[1] - x[0], .height = fsz.h});
+	LOG("invert_r(current: %d, i)", current);
+	if (current <= i->end_point.row) {
+		const uint8_t fw = i->font_size.width, fh =
+			i->font_size.height;
+		const int16_t x[] = {current == i->start_point.row ?
+			i->start_point.columns * fw : 0,
+			(current == i->end_point.row ? i->end_point.columns
+			 : i->width_in_chars) * fw};
+		xcb_poly_fill_rectangle(i->xc, i->window, i->gc, 1,
+			&(xcb_rectangle_t){.x = x[0], .y = current * fh,
+			.width = x[1] - x[0], .height = fh});
+		invert_r(current + 1, i);
 	}
-}
-// Make sure the first row is not before the beginning of the screen.
-static inline uint8_t get_start_row(const int16_t rs)
-{
-	return rs < 0 ? 0 : rs;
-}
-// Make sure last row is not past the end of the screen.
-static inline uint8_t get_end_row(const int16_t re)
-{
-	const uint16_t h = jbxvt_get_char_size().h;
-	return re >= h ? h - 1 : re;
 }
 static void change(xcb_connection_t * restrict xc, struct JBDim * se,
 	struct JBDim * ose)
 {
+	LOG("change()");
 	const int16_t n = jbxvt_selcmp(se, ose);
 	if (!n) // nothing selected
 		return;
@@ -48,9 +48,16 @@ static void change(xcb_connection_t * restrict xc, struct JBDim * se,
 		start = jbxvt_get_selend_position(nn ? se : ose);
 		end = jbxvt_get_selend_position(nn ? ose : se);
 	}
+	struct InversionData i = {.xc = xc,
+		.font_size = jbxvt_get_font_size(),
+		.start_point = start, .end_point = end,
+		.window = jbxvt_get_vt_window(xc),
+		.gc = jbxvt_get_cursor_gc(xc),
+		.width_in_chars = jbxvt_get_char_size().width,
+	};
 	// Invert the changed area
-	invert(xc, start.row, end.row, start.col, end.col,
-		get_start_row(start.row), get_end_row(end.row));
+	invert_r(i.start_point.row, &i);
+
 }
 /*  Repaint the displayed selection to reflect the new value.
     ose1 and ose2 are assumed to represent the currently
