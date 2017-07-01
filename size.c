d74311d71bbeaf5690ac2a54cba32e3ad6810b41
Jeffrey E. Bedard <jefbed@gmail.com>
2017-06-30 23:04:23 -0400

    Saved size_in_chars when pixel size changed instead of computing it each time it is requested.

--- size.c
+++ size.c
@@ -2,10 +2,11 @@
 #include "size.h"
 #include "font.h"
 #include "libjb/JBDim.h"
-static struct JBDim size_in_pixels;
+static struct JBDim size_in_pixels, size_in_chars;
 void jbxvt_set_pixel_size(const struct JBDim size)
 {
 	size_in_pixels = size;
+	size_in_chars = jbxvt_pixels_to_chars(size);
 }
 // Return size module's stored pixel size
 struct JBDim jbxvt_get_pixel_size(void)
@@ -15,7 +16,7 @@ struct JBDim jbxvt_get_pixel_size(void)
 // Return size module's stored size converted to characters
 struct JBDim jbxvt_get_char_size(void)
 {
-	return jbxvt_pixels_to_chars(size_in_pixels);
+	return size_in_chars;
 }
 // Convert pixel size/position to char size/position
 struct JBDim jbxvt_pixels_to_chars(struct JBDim p)
