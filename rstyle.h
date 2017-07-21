b51cb705b9f9fb18528ef514afb8fd171b86eb77
Jeffrey E. Bedard <jefbed@gmail.com>
2017-07-21 13:55:40 -0400

    Added comment explaining use of typedef to stdint type vs enum JBXVTRenderStyle.

--- rstyle.h
+++ rstyle.h
@@ -2,6 +2,8 @@
 #ifndef JBXVT_RSTYLE_H
 #define JBXVT_RSTYLE_H
 #include <stdint.h>
+/* Use the stdint type here instead of a typedef to enum JBXVTRenderStyle in
+ * order to ensure 32 bits cross platform.  */
 typedef uint32_t rstyle_t;
 void jbxvt_add_rstyle(const rstyle_t val);
 void jbxvt_del_rstyle(const rstyle_t val);
