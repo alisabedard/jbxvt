d06560e7b7d5d18468a2df9e595db406c05bcd2a
Jeffrey E. Bedard <jefbed@gmail.com>
2017-07-22 01:02:16 -0400

    Edited.

--- rstyle.h
+++ rstyle.h
@@ -3,7 +3,7 @@
 #define JBXVT_RSTYLE_H
 #include <stdint.h>
 /* Use the stdint type here instead of a typedef to enum JBXVTRenderStyle in
- * order to ensure 32 bits cross platform.  */
+ * order to ensure 32 bits across platforms.  */
 typedef uint32_t rstyle_t;
 void jbxvt_add_rstyle(const rstyle_t val);
 void jbxvt_del_rstyle(const rstyle_t val);
