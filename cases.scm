576cfa2a4702d4955105612f631c2f87c84946be
Jeffrey E. Bedard <jefbed@gmail.com>
2017-07-28 03:23:57 -0400

    Used hypen instead of underscore in function names. Used mit-scheme's format to generate strings instead of string-append in get-case, get-log, get-fixme, and get-handler. Split line in casegen.

--- cases.scm
+++ cases.scm
@@ -1,6 +1,7 @@
 (define copyright "Copyright 2017, Jeffrey E. Bedard")
 ; Written for mit-scheme
 ; vim: sw=2
+(load-option 'format)
 (define what_type?
  (lambda (type) ; map chars to symbols
   (cond ((char=? type #\l) 'logged)
@@ -8,26 +9,22 @@
    ((char=? type #\s) 'stub)
    (else 'bad))))
 ; format components:
-(define get_case (lambda (token) (string-append "case " token ":\n")))
-(define get_log (lambda (message) (string-append "\tLOG(\"" message
-				   "\");\n")))
-(define get_fixme
- (lambda (token) (get_log (string-append
-			   "FIXME: " token " not implemented."))))
-(define get_handler
- (lambda (token)
-  (string-append "\tjbxvt_handle_" token "(xc, &token);\n")))
-(define get_formatted
+(define (get-case tkn) (format #f "case ~A:~%" tkn))
+(define (get-log msg) (format #f "\tLOG(\"~A\");~%" msg))
+(define (get-fixme tkn)
+ (get-log (format #f "FIXME: ~A not implemented." tkn)))
+(define (get-handler tkn) (format #f "\tjbxvt_handle_~A(xc, &token);~%" tkn))
+(define get-formatted
  (lambda (token type) ; map symbols to formats
-  (string-append (get_case token )
+  (string-append (get-case token )
    (cond ((equal? type 'logged) (begin
-				 (string-append (get_log token)
-				  (get_handler token))))
-    ((equal? type 'unlogged) (get_handler token))
-    (else (get_fixme token))) "\tbreak;\n")))
-(define get_token
+				 (string-append (get-log token)
+				  (get-handler token))))
+    ((equal? type 'unlogged) (get-handler token))
+    (else (get-fixme token))) "\tbreak;\n")))
+(define get-token
  (lambda (line i) (string-append "JBXVT_TOKEN_" (string-head line i))))
-(define get_type
+(define get-type
  (lambda (line i) (what_type? (string-ref (string-tail line (+ 1 i)) 0))))
 (define parse
  (lambda (in out)
@@ -35,14 +32,16 @@
    ((line (read-line in))
     ((not (eof-object? line))))
    (let ((i (string-find-next-char line #\:)))
-    (let ((token (get_token line i))
-	  (type (get_type line i)))
-     (display (get_formatted token type) out)
+    (let ((token (get-token line i))
+	  (type (get-type line i)))
+     (display (get-formatted token type) out)
      (parse in out))))))
 (define casegen
  (lambda (in_file_name out_file_name)
   (let*
    ((i (open-input-file in_file_name))
     (o (open-output-file out_file_name)))
-   (parse i o) (close-port i) (close-port o))))
+   (parse i o)
+   (close-port i)
+   (close-port o))))
 (casegen "cases.txt" "cases.c")
