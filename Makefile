1c261cd25641330bc0fac348d31fa3217afc45c4
Jeffrey E. Bedard <jefbed@gmail.com>
2017-08-05 17:55:23 -0400

    Adjusted terminfo naming to adapt to vim's simplistic detection of mouse support.

--- Makefile
+++ Makefile
@@ -39,7 +39,7 @@ bindest=${DESTDIR}${PREFIX}/bin
 docdest=${DESTDIR}${PREFIX}/share/man/man1
 terminfo:
 	tic -xs jbxvt.terminfo
-	chmod 0644 /usr/share/terminfo/j/jbxvt # Make it readable
+	chmod 0644 /usr/share/terminfo/x/xterm-jbxvt # Make it readable
 install: ${exe} terminfo
 	install -d ${bindest}
 	install ${exe} ${bindest}
--- config.h
+++ config.h
@@ -13,7 +13,7 @@
 /* FIXME:  Even though we provide a jbxvt terminfo entry, mouse tracking in
  * vim does not work with it.  Set TERM to xterm before launching vim or fix
  * vim.  */
-#define JBXVT_ENV_TERM "jbxvt"
+#define JBXVT_ENV_TERM "xterm-jbxvt"
 #define JBXVT_ENV_SHELL "SHELL"
 #define JBXVT_FOREGROUND_COLOR "white"
 #define JBXVT_BACKGROUND_COLOR "black"
